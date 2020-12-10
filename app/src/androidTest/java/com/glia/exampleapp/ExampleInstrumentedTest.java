cf03e7612156d71814790865305f347dd0d917fc
Aleksandr Chatsky <aleksandr.chatsky@glia.com>
2020-12-10 12:20:23 +0200

    Fix UI tests

--- app/src/androidTest/java/com/glia/exampleapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/glia/exampleapp/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.glia.widgetssdk", appContext.getPackageName());
+        assertEquals("com.glia.exampleapp.debug", appContext.getPackageName());
     }
 }

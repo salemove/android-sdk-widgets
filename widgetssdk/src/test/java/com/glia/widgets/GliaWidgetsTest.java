650e9930b6288e7b5b58d6367dcadca262b4dcab
Andrii Horishnii <andrii.horishnii@glia.com>
2022-02-09 12:21:26 +0200

    Add simple unit tests

---
+++ widgetssdk/src/test/java/com/glia/widgets/GliaWidgetsTest.java
@@ -0,0 +1,66 @@
+package com.glia.widgets;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.glia.androidsdk.GliaConfig;
+import com.glia.androidsdk.SiteApiKey;
+import com.glia.widgets.di.ControllerFactory;
+import com.glia.widgets.di.Dependencies;
+import com.glia.widgets.di.GliaCore;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+public class GliaWidgetsTest {
+
+    private GliaCore gliaCore;
+    private ControllerFactory controllerFactory;
+
+    @Before
+    public void setUp() {
+        gliaCore = mock(GliaCore.class);
+        Dependencies.setGlia(gliaCore);
+
+        controllerFactory = mock(ControllerFactory.class);
+        Dependencies.setControllerFactory(controllerFactory);
+    }
+
+    @Test
+    public void onAppCreate_setApplicationToGliaCore_whenCalled() {
+        Application application = mock(Application.class);
+
+        GliaWidgets.onAppCreate(application);
+
+        verify(gliaCore).onAppCreate(application);
+    }
+
+    @Test
+    public void init_setConfigToGliaCore_whenCalled() {
+        SiteApiKey siteApiKey = new SiteApiKey("SiteApiId", "SiteApiSecret");
+        String siteId = "SiteId";
+        String region = "Region";
+        Context context = mock(Context.class);
+        GliaWidgetsConfig widgetsConfig = new GliaWidgetsConfig.Builder()
+                .setSiteApiKey(siteApiKey)
+                .setSiteId(siteId)
+                .setRegion(region)
+                .setContext(context)
+                .build();
+
+        GliaWidgets.init(widgetsConfig);
+
+        ArgumentCaptor<GliaConfig> captor = ArgumentCaptor.forClass(GliaConfig.class);
+        verify(gliaCore).init(captor.capture());
+        GliaConfig gliaConfig = captor.getValue();
+        assertEquals(siteApiKey, gliaConfig.getSiteApiKey());
+        assertEquals(siteId, gliaConfig.getSiteId());
+        assertEquals(region, gliaConfig.getRegion());
+        assertEquals(context, gliaConfig.getContext());
+    }
+}

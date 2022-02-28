6a7998e6624d844abfd089a2c7e2068d903a241f
Andrii Horishnii <andrii.horishnii@glia.com>
2022-02-28 21:58:40 +0200

    Cover CallView with UI tests
    MOB-1139

--- widgetssdk/src/androidTest/java/com/glia/widgets/ExampleInstrumentedTest.java
+++ widgetssdk/src/androidTest/java/com/glia/widgets/ExampleInstrumentedTest.java
@@ -1,7 +1,15 @@
 package com.glia.widgets;
 
+import static androidx.test.core.app.ActivityScenario.launch;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
 import android.content.Context;
 
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -9,6 +17,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.glia.widgets.di.ControllerFactory;
+import com.glia.widgets.di.Dependencies;
+import com.glia.widgets.filepreview.ui.FilePreviewActivity;
+import com.glia.widgets.filepreview.ui.FilePreviewController;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -22,5 +37,21 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.glia.widgets.test", appContext.getPackageName());
+    }
+
+    // just example
+    @Test
+    public void testFilePreview() throws InterruptedException {
+        FilePreviewController filePreviewController = mock(FilePreviewController.class);
+        ControllerFactory controllerFactory = mock(ControllerFactory.class);
+        when(controllerFactory.getImagePreviewController()).thenReturn(filePreviewController);
+        Dependencies.setControllerFactory(controllerFactory);
+
+        ActivityScenario<FilePreviewActivity> scenario = launch(FilePreviewActivity.class);
+        scenario.moveToState(Lifecycle.State.RESUMED);
+
+        Thread.sleep(10); // sleep if needs
+
+        onView(withId(R.id.file_preview_view)).check(matches(isDisplayed()));
     }
 }

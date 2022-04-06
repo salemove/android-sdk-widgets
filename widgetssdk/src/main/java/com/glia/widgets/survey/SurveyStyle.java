4b931ec914d3d92d645f96defc1861b1344beb9d
andrews <av.shevtsov@gmail.com>
2022-04-06 19:10:07 +0300

    Create builder for SurveySingleOptionConfiguration, move to a separate package.

--- widgetssdk/src/main/java/com/glia/widgets/survey/SurveyStyle.java
+++ widgetssdk/src/main/java/com/glia/widgets/survey/SurveyStyle.java
@@ -8,7 +8,7 @@ import com.glia.widgets.helper.ResourceProvider;
 import com.glia.widgets.view.configuration.SurveyBooleanOption;
 import com.glia.widgets.view.configuration.SurveyInputOption;
 import com.glia.widgets.view.configuration.SurveyScaleOption;
-import com.glia.widgets.view.configuration.SurveySingleOption;
+import com.glia.widgets.view.configuration.survey.SurveySingleOptionConfiguration;
 import com.glia.widgets.view.configuration.Text;
 
 public class SurveyStyle implements Parcelable {
@@ -16,7 +16,7 @@ public class SurveyStyle implements Parcelable {
     private Text title;
     private SurveyBooleanOption booleanOption;
     private SurveyScaleOption scaleOption;
-    private SurveySingleOption singleOption;
+    private SurveySingleOptionConfiguration singleOption;
     private SurveyInputOption inputOption;
 
     public String getBgColor() {
@@ -35,7 +35,7 @@ public class SurveyStyle implements Parcelable {
         return scaleOption;
     }
 
-    public SurveySingleOption getSingleOption() {
+    public SurveySingleOptionConfiguration getSingleOption() {
         return singleOption;
     }
 
@@ -43,9 +43,7 @@ public class SurveyStyle implements Parcelable {
         return inputOption;
     }
 
-    private SurveyStyle(
-            Builder builder
-    ) {
+    private SurveyStyle(Builder builder) {
         this.bgColor = builder.bgColor;
         this.title = builder.title;
         this.booleanOption = builder.booleanOption;
@@ -59,7 +57,7 @@ public class SurveyStyle implements Parcelable {
         private Text title;
         private SurveyBooleanOption booleanOption;
         private SurveyScaleOption scaleOption;
-        private SurveySingleOption singleOption;
+        private SurveySingleOptionConfiguration singleOption;
         private SurveyInputOption inputOption;
 
         public Builder(ResourceProvider resourceProvider) {
@@ -87,7 +85,7 @@ public class SurveyStyle implements Parcelable {
             return this;
         }
 
-        public Builder singleOption(SurveySingleOption singleOption) {
+        public Builder singleOption(SurveySingleOptionConfiguration singleOption) {
             this.singleOption = singleOption;
             return this;
         }
@@ -123,7 +121,7 @@ public class SurveyStyle implements Parcelable {
         this.title = source.readParcelable(Text.class.getClassLoader());
         this.booleanOption = source.readParcelable(SurveyBooleanOption.class.getClassLoader());
         this.scaleOption = source.readParcelable(SurveyScaleOption.class.getClassLoader());
-        this.singleOption = source.readParcelable(SurveySingleOption.class.getClassLoader());
+        this.singleOption = source.readParcelable(SurveySingleOptionConfiguration.class.getClassLoader());
         this.inputOption = source.readParcelable(SurveyInputOption.class.getClassLoader());
     }
 
@@ -132,7 +130,7 @@ public class SurveyStyle implements Parcelable {
         this.title = in.readParcelable(Text.class.getClassLoader());
         this.booleanOption = in.readParcelable(SurveyBooleanOption.class.getClassLoader());
         this.scaleOption = in.readParcelable(SurveyScaleOption.class.getClassLoader());
-        this.singleOption = in.readParcelable(SurveySingleOption.class.getClassLoader());
+        this.singleOption = in.readParcelable(SurveySingleOptionConfiguration.class.getClassLoader());
         this.inputOption = in.readParcelable(SurveyInputOption.class.getClassLoader());
     }
 
---
+++ widgetssdk/src/main/java/com/glia/widgets/view/configuration/survey/SurveySingleOptionConfiguration.java
@@ -0,0 +1,85 @@
+package com.glia.widgets.view.configuration.survey;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.glia.widgets.view.configuration.FontConfiguration;
+import com.glia.widgets.view.configuration.Text;
+
+public class SurveySingleOptionConfiguration implements Parcelable {
+    private Text title;
+    private String highlightedColor; /* use for border and not-selected option on validation */
+    private FontConfiguration textFieldFont;
+
+    private SurveySingleOptionConfiguration(Builder builder) {
+        this.title = builder.title;
+        this.highlightedColor = builder.highlightedColor;
+        this.textFieldFont = builder.textFieldFont;
+    }
+
+    public static class Builder {
+        private Text title;
+        private String highlightedColor; /* use for border and not-selected option on validation */
+        private FontConfiguration textFieldFont;
+
+        public Builder(){
+        }
+
+        public Builder title(Text title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder highlightedColor(String highlightedColor) {
+            this.highlightedColor = highlightedColor;
+            return this;
+        }
+
+        public Builder textFieldFont(FontConfiguration textFieldFont) {
+            this.textFieldFont = textFieldFont;
+            return this;
+        }
+
+        public SurveySingleOptionConfiguration build() {
+            return new SurveySingleOptionConfiguration(this);
+        }
+    }
+
+    /* BEGIN: Parcelable related */
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeParcelable(this.title, flags);
+        dest.writeString(this.highlightedColor);
+        dest.writeParcelable(this.textFieldFont, flags);
+    }
+
+    public void readFromParcel(Parcel source) {
+        this.title = source.readParcelable(Text.class.getClassLoader());
+        this.highlightedColor = source.readString();
+        this.textFieldFont = source.readParcelable(FontConfiguration.class.getClassLoader());
+    }
+
+    protected SurveySingleOptionConfiguration(Parcel in) {
+        this.title = in.readParcelable(Text.class.getClassLoader());
+        this.highlightedColor = in.readString();
+        this.textFieldFont = in.readParcelable(FontConfiguration.class.getClassLoader());
+    }
+
+    public static final Creator<SurveySingleOptionConfiguration> CREATOR = new Creator<SurveySingleOptionConfiguration>() {
+        @Override
+        public SurveySingleOptionConfiguration createFromParcel(Parcel source) {
+            return new SurveySingleOptionConfiguration(source);
+        }
+
+        @Override
+        public SurveySingleOptionConfiguration[] newArray(int size) {
+            return new SurveySingleOptionConfiguration[size];
+        }
+    };
+    /* END: Parcelable related */
+}

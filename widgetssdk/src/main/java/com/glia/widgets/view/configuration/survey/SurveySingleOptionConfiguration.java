11a4771faa25f0388eee12d4b4c2fff3e98d7676
Andriy Shevtsov <andriy.shevtsov@masterofcode.com>
2022-04-13 02:01:33 +0300

    Add default configuration to SurveySingleOptionConfiguration

--- widgetssdk/src/main/java/com/glia/widgets/view/configuration/survey/SurveySingleOptionConfiguration.java
+++ widgetssdk/src/main/java/com/glia/widgets/view/configuration/survey/SurveySingleOptionConfiguration.java
@@ -1,8 +1,11 @@
 package com.glia.widgets.view.configuration.survey;
 
+import android.annotation.SuppressLint;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.glia.widgets.R;
+import com.glia.widgets.helper.ResourceProvider;
 import com.glia.widgets.view.configuration.FontConfiguration;
 import com.glia.widgets.view.configuration.TextRuntimeConfiguration;
 
@@ -34,7 +37,20 @@ public class SurveySingleOptionConfiguration implements Parcelable {
         private String highlightedColor; /* use for border and not-selected option on validation */
         private FontConfiguration textFieldFont;
 
-        public Builder(){
+        @SuppressLint("ResourceType")
+        public Builder(ResourceProvider resourceProvider) {
+            FontConfiguration titleFont = new FontConfiguration(
+                    FontConfiguration.FontSize.BODY,
+                    FontConfiguration.FontWeight.MEDIUM);
+            String titleColorString = resourceProvider.getString(R.color.glia_base_dark_color);
+            this.title = new TextRuntimeConfiguration(
+                    titleFont,
+                    titleColorString,
+                    titleColorString);
+            this.highlightedColor = resourceProvider.getString(R.color.glia_system_negative_color);
+            this.textFieldFont = new FontConfiguration(
+                    FontConfiguration.FontSize.BODY,
+                    FontConfiguration.FontWeight.REGULAR);
         }
 
         public Builder title(TextRuntimeConfiguration title) {

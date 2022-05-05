14163b008a66ed21b4d280ad68587554c2f3dd15
Andrii Horishnii <andrii.horishnii@glia.com>
2022-05-05 13:31:35 +0300

    Make dynamic the survey screen title.

--- widgetssdk/src/main/java/com/glia/widgets/survey/SurveyState.java
+++ widgetssdk/src/main/java/com/glia/widgets/survey/SurveyState.java
@@ -1,15 +1,49 @@
 package com.glia.widgets.survey;
 
+import androidx.annotation.Nullable;
+
 import java.util.List;
 
 public class SurveyState {
+    @Nullable
+    public final String title;
+
+    @Nullable
     public final List<QuestionItem> questions;
 
-    public SurveyState() {
-        this.questions = null;
+    private SurveyState(String title, List<QuestionItem> questions) {
+        this.title = title;
+        this.questions = questions;
     }
 
-    public SurveyState(List<QuestionItem> questions) {
-        this.questions = questions;
+    static class Builder {
+        @Nullable
+        public String title;
+
+        @Nullable
+        public List<QuestionItem> questions;
+
+        public Builder setTitle(@Nullable String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder setQuestions(@Nullable List<QuestionItem> questions) {
+            this.questions = questions;
+            return this;
+        }
+
+        public Builder copyFrom(@Nullable SurveyState state) {
+            if (state == null) {
+                return this;
+            }
+            this.title = state.title;
+            this.questions = state.questions;
+            return this;
+        }
+
+        public SurveyState createSurveyState() {
+            return new SurveyState(title, questions);
+        }
     }
 }

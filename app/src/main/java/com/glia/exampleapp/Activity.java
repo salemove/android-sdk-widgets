3e48c3222d62e45b1586467e1c3e1d456647dff0
Davit Dolmazyan <davit.dolmazyan@glia.com>
2022-06-07 15:14:16 +0300

    Devs want to implement deep-link navigation to widgets SDK
    MOB-1341

--- app/src/main/java/com/glia/exampleapp/Activity.java
+++ app/src/main/java/com/glia/exampleapp/Activity.java
@@ -4,11 +4,22 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.glia.androidsdk.Glia;
+import com.glia.widgets.GliaWidgets;
+
 public class Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity);
+
+        initGliaWidgets();
+    }
+
+    private void initGliaWidgets() {
+        if (Glia.isInitialized()) return;
+
+        GliaWidgets.init(GliaWidgetsConfigManager.obtainConfig(getIntent(), getApplicationContext()));
     }
 }

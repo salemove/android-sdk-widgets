459b7e2a9ae9b80cea39e056f60f49c5b603c187
Andrii Horishnii <andrii.horishnii@masterofcode.com>
2022-07-01 17:45:05 +0300

    Remove app-token settings from TestingApp (Widgets)
    MOB-1179

--- app/src/main/java/com/glia/exampleapp/SettingsFragment.java
+++ app/src/main/java/com/glia/exampleapp/SettingsFragment.java
@@ -1,20 +1,13 @@
 package com.glia.exampleapp;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.PreferenceManager;
 
-import com.glia.exampleapp.auth.AuthorizationType;
 import com.glia.widgets.GliaWidgets;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
-
-    private SharedPreferences sharedPreferences;
 
     public SettingsFragment() {
     }
@@ -22,8 +15,6 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext());
-        initAuthorizationPreferenceClickListener();
         setPreferenceDescription(R.string.pref_glia_core_sdk_version, GliaWidgets.getWidgetsCoreSdkVersion());
         setPreferenceDescription(R.string.pref_glia_widgets_sdk_version, GliaWidgets.getWidgetsSdkVersion());
     }
@@ -31,7 +22,6 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onResume() {
         super.onResume();
-        updateAuthorizationDescription();
     }
 
     private void setPreferenceDescription(int preferenceId, String value) {
@@ -41,34 +31,4 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         }
     }
 
-    private void initAuthorizationPreferenceClickListener() {
-        NavController navController = NavHostFragment.findNavController(this);
-        Preference authorizationButtonPreference = findPreference(getString(R.string.pref_app_authorization));
-        if (authorizationButtonPreference != null)
-            authorizationButtonPreference.setOnPreferenceClickListener(preference -> {
-                navController.navigate(R.id.authorization);
-                return true;
-            });
-    }
-
-    private void updateAuthorizationDescription() {
-        int authorizationType = sharedPreferences.getInt(getString(R.string.pref_authorization_type), AuthorizationType.DEFAULT);
-        Preference preference = findPreference(getString(R.string.pref_app_authorization));
-        if (preference != null) setAuthorizationSummary(preference, authorizationType);
-    }
-
-    private void setAuthorizationSummary(Preference preference, int authorizationType) {
-        if (authorizationType == AuthorizationType.APP_TOKEN)
-            setAppTokenSummary(preference);
-        else if (authorizationType == AuthorizationType.SITE_API_KEY)
-            setApiKeySummary(preference);
-    }
-
-    private void setAppTokenSummary(Preference preference) {
-        preference.setSummary(R.string.authorization_app_token);
-    }
-
-    private void setApiKeySummary(Preference preference) {
-        preference.setSummary(R.string.authorization_site_api_key);
-    }
 }

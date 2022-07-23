4b0b795e7c2ca565e92e5fb913671f0e0adb4867
Davit Dolmazyan <davit.dolmazyan@glia.com>
2022-07-23 16:49:42 +0300

    Disable sending message during engagement transferring
    MOB-1456

--- widgetssdk/src/main/java/com/glia/widgets/chat/model/ChatInputMode.java
+++ widgetssdk/src/main/java/com/glia/widgets/chat/model/ChatInputMode.java
@@ -1,5 +1,13 @@
 package com.glia.widgets.chat.model;
 
 public enum ChatInputMode {
-    ENABLED_NO_ENGAGEMENT, ENABLED, SINGLE_CHOICE_CARD
+    ENABLED_NO_ENGAGEMENT,
+    ENABLED,
+    SINGLE_CHOICE_CARD,
+    DISABLED;
+
+    public boolean isEnabled() {
+        return this == ENABLED_NO_ENGAGEMENT || this == ENABLED;
+    }
+
 }

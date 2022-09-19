83358dd45faebc92d146b48403c023df82aa0704
Gerson Noboa <gerson.noboa@glia.com>
2022-09-19 11:55:59 +0300

    Add workflow to increment Core SDK version
    Using the previous work done in Gradle, a new workflow has been added
    to `bitrise.yml` that increments the Core SDK version. A pull request
    is created also by directly calling the GitHub API. This will be used
    in the Core SDK release process, in order to increment the Core SDK
    version used in the widgets once the Core SDK is released. This
    workflow requires an environment variable called `NEW_VERSION`.
    
    MOB-1510

---
+++ scripts/create-pr-for-increment-project-version.sh
@@ -0,0 +1,17 @@
+ #!/bin/bash
+
+NEW_VERSION=$1
+BRANCH_NAME="version-update/${NEW_VERSION}"
+MESSAGE="Update project version to ${NEW_VERSION}"
+
+git checkout -b $BRANCH_NAME
+git add -A
+git commit -m "$MESSAGE"
+git push origin "$BRANCH_NAME":"$BRANCH_NAME"
+
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_API_TOKEN" \
+  https://api.github.com/repos/salemove/android-sdk-widgets/pulls \
+  -d "{\"title\":\"${MESSAGE}\",\"head\":\"${BRANCH_NAME}\",\"base\":\"master\"}"

8aaa0b324b76ca07146ec8b52ec3fec4a80a1089
Davit Dolmazyan <davit.dolmazyan@glia.com>
2023-01-06 00:24:57 +0200

    Response card improvements
    MOB-1566

--- widgetssdk/src/main/java/com/glia/widgets/chat/model/history/OperatorMessageItem.java
+++ widgetssdk/src/main/java/com/glia/widgets/chat/model/history/OperatorMessageItem.java
@@ -1,18 +1,14 @@
 package com.glia.widgets.chat.model.history;
 
-import com.glia.androidsdk.chat.SingleChoiceOption;
+import androidx.annotation.NonNull;
+
 import com.glia.widgets.chat.adapter.ChatAdapter;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Objects;
 
 public class OperatorMessageItem extends OperatorChatItem {
     public final String operatorName;
     public final String content;
-    public final List<SingleChoiceOption> singleChoiceOptions;
-    public final Integer selectedChoiceIndex;
-    public final String choiceCardImageUrl;
 
     public OperatorMessageItem(
             String id,
@@ -20,18 +16,12 @@ public class OperatorMessageItem extends OperatorChatItem {
             String operatorProfileImgUrl,
             boolean showChatHead,
             String content,
-            List<SingleChoiceOption> singleChoiceOptions,
-            Integer selectedChoiceIndex,
-            String choiceCardImageUrl,
             String operatorId,
             long timestamp
     ) {
         super(id, ChatAdapter.OPERATOR_MESSAGE_VIEW_TYPE, showChatHead, operatorProfileImgUrl, operatorId, id, timestamp);
         this.operatorName = operatorName;
         this.content = content;
-        this.singleChoiceOptions = singleChoiceOptions != null ? Collections.unmodifiableList(singleChoiceOptions) : null;
-        this.selectedChoiceIndex = selectedChoiceIndex;
-        this.choiceCardImageUrl = choiceCardImageUrl;
     }
 
     @Override
@@ -41,17 +31,15 @@ public class OperatorMessageItem extends OperatorChatItem {
         if (!super.equals(o)) return false;
         OperatorMessageItem that = (OperatorMessageItem) o;
         return Objects.equals(operatorName, that.operatorName)
-                && Objects.equals(content, that.content)
-                && Objects.equals(singleChoiceOptions, that.singleChoiceOptions)
-                && Objects.equals(selectedChoiceIndex, that.selectedChoiceIndex)
-                && Objects.equals(choiceCardImageUrl, that.choiceCardImageUrl);
+                && Objects.equals(content, that.content);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), operatorName, content, singleChoiceOptions, selectedChoiceIndex, choiceCardImageUrl);
+        return Objects.hash(super.hashCode(), operatorName, content);
     }
 
+    @NonNull
     @Override
     public String toString() {
         return "OperatorMessageItem{" +
@@ -60,9 +48,6 @@ public class OperatorMessageItem extends OperatorChatItem {
                 ", operatorId='" + operatorId + '\'' +
                 ", operatorName='" + operatorName + '\'' +
                 ", content='" + content + '\'' +
-                ", singleChoiceOptions=" + singleChoiceOptions +
-                ", selectedChoiceIndex=" + selectedChoiceIndex +
-                ", choiceCardImageUrl='" + choiceCardImageUrl + '\'' +
                 "} " + super.toString();
     }
 }

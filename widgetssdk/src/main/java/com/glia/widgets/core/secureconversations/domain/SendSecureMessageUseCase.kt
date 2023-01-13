838a82067e733c3f1c16ecd9a33ad4a3cdc30523
Andrii Horishnii <andrii.horishnii@masterofcode.com>
2023-01-13 20:12:33 +0200

    Added file attachment functionality to secure conversations welcome screen

--- widgetssdk/src/main/java/com/glia/widgets/core/secureconversations/domain/SendSecureMessageUseCase.kt
+++ widgetssdk/src/main/java/com/glia/widgets/core/secureconversations/domain/SendSecureMessageUseCase.kt
@@ -1,12 +1,16 @@
 package com.glia.widgets.core.secureconversations.domain
 
 import com.glia.androidsdk.RequestCallback
+import com.glia.androidsdk.chat.FilesAttachment
 import com.glia.androidsdk.chat.VisitorMessage
+import com.glia.widgets.core.fileupload.SecureFileAttachmentRepository
+import com.glia.widgets.core.fileupload.model.FileAttachment
 import com.glia.widgets.core.secureconversations.SecureConversationsRepository
 
 class SendSecureMessageUseCase(
     private val queueId: String,
-    private val secureConversationsRepository: SecureConversationsRepository
+    private val secureConversationsRepository: SecureConversationsRepository,
+    private val fileAttachmentRepository: SecureFileAttachmentRepository
 ) {
 
     fun execute(
@@ -14,6 +18,26 @@ class SendSecureMessageUseCase(
         callback: RequestCallback<VisitorMessage?>
     ) {
         val queueIds = arrayOf(queueId)
-        secureConversationsRepository.send(message, queueIds, callback)
+        val fileAttachments = fileAttachmentRepository.getReadyToSendFileAttachments()
+        if (fileAttachments != null && fileAttachments.isNotEmpty()) {
+            sendMessageWithAttachments(message, queueIds, fileAttachments, callback)
+        } else {
+            secureConversationsRepository.send(message, queueIds, callback)
+        }
+    }
+
+    private fun sendMessageWithAttachments(
+        message: String,
+        queueIds: Array<String>,
+        fileAttachments: List<FileAttachment>,
+        callback: RequestCallback<VisitorMessage?>
+    ) {
+        val filesAttachment = fileAttachments
+            .map { it.engagementFile }
+            .toTypedArray()
+            .let { FilesAttachment.from(it) }
+
+        secureConversationsRepository.send(message, queueIds, filesAttachment, callback)
+        fileAttachmentRepository.detachFiles(fileAttachments)
     }
 }

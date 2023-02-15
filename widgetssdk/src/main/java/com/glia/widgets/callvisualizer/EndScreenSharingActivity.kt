fde01e2259dce8e9d232f5b8cdde72015b9d3920
Karl Valliste <karl.valliste@gmail.com>
2023-02-15 12:34:06 +0200

    Created end screen sharing
    MOB-1861

---
+++ widgetssdk/src/main/java/com/glia/widgets/callvisualizer/EndScreenSharingActivity.kt
@@ -0,0 +1,28 @@
+package com.glia.widgets.callvisualizer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.glia.widgets.databinding.EndScreenSharingActivityBinding
+import com.glia.widgets.di.Dependencies
+
+class EndScreenSharingActivity : AppCompatActivity(), EndScreenSharingView.OnFinishListener {
+
+    private lateinit var binding: EndScreenSharingActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = EndScreenSharingActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val screenSharingView = binding.screenSharingScreenView
+        screenSharingView.onFinishListener = this
+
+        val controller = Dependencies.getControllerFactory().screenSharingViewController
+        screenSharingView.setController(controller)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.screenSharingScreenView.onDestroy()
+    }
+}
---
+++ widgetssdk/src/test/java/com/glia/widgets/callvisualizer/EndScreenSharingControllerTest.kt
@@ -0,0 +1,44 @@
+package com.glia.widgets.callvisualizer
+
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.verifyNoInteractions
+import org.mockito.Mockito.verifyNoMoreInteractions
+
+internal class ScreenSharingScreenControllerTest {
+
+    private val controller = EndScreenSharingController()
+    private val view = mock(EndScreenSharingContract.View::class.java)
+
+    @Before
+    fun setup() {
+        controller.setView(view)
+    }
+
+    @Test
+    fun setView() {
+        verifyNoInteractions(view)
+    }
+
+    @Test
+    fun onBackArrowClicked() {
+        controller.onBackArrowClicked()
+        verify(view).finish()
+        verifyNoMoreInteractions(view)
+    }
+
+    @Test
+    fun onEndScreenSharingButtonClicked() {
+        controller.onEndScreenSharingButtonClicked()
+        verify(view).stopScreenSharing()
+        verifyNoMoreInteractions(view)
+    }
+
+    @Test
+    fun onDestroy() {
+        controller.onDestroy()
+        verifyNoInteractions(view)
+    }
+}

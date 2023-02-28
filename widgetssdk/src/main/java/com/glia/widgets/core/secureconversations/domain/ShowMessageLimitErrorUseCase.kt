2b7b3d2ae92913fb79cbd12575fe0b857371d6a2
Andrii Horishnii <andrii.horishnii@masterofcode.com>
2023-02-28 23:04:40 +0200

    Update the Welcome to Message Center views logic
    MOB-1893

---
+++ widgetssdk/src/main/java/com/glia/widgets/core/secureconversations/domain/ShowMessageLimitErrorUseCase.kt
@@ -0,0 +1,20 @@
+package com.glia.widgets.core.secureconversations.domain
+
+import com.glia.widgets.core.secureconversations.SendMessageRepository
+import com.glia.widgets.helper.rx.Schedulers
+import io.reactivex.Observable
+
+class ShowMessageLimitErrorUseCase(
+    private val sendMessageRepository: SendMessageRepository,
+    private val schedulers: Schedulers
+) {
+
+    operator fun invoke(): Observable<Boolean> = sendMessageRepository.observable
+            .map { it.count() > MAX_MESSAGE_LENGTH }
+            .subscribeOn(schedulers.computationScheduler)
+            .observeOn(schedulers.mainScheduler)
+
+    companion object {
+        private const val MAX_MESSAGE_LENGTH = 10000
+    }
+}

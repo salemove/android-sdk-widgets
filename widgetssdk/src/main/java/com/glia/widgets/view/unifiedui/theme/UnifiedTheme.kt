4d90d4ce5fc0411c8bc35eef73cb0e2cd0768a5d
Davit Dolmazyan <davit.dolmazyan@glia.com>
2023-03-02 15:51:29 +0200

    Create models/data classes for secure conversation welcome screen (#468)
    * Create models/data classes for secure conversation welcome screen according to JSON schema
    
    MOB 1894
    
    * fixup! Create models/data classes for secure conversation welcome screen according to JSON schema

--- widgetssdk/src/main/java/com/glia/widgets/view/unifiedui/theme/UnifiedTheme.kt
+++ widgetssdk/src/main/java/com/glia/widgets/view/unifiedui/theme/UnifiedTheme.kt
@@ -5,6 +5,7 @@ import com.glia.widgets.view.unifiedui.theme.bubble.BubbleTheme
 import com.glia.widgets.view.unifiedui.theme.call.CallTheme
 import com.glia.widgets.view.unifiedui.theme.callvisulaizer.CallVisualizerTheme
 import com.glia.widgets.view.unifiedui.theme.chat.ChatTheme
+import com.glia.widgets.view.unifiedui.theme.secureconversations.SecureConversationsWelcomeScreenTheme
 import com.glia.widgets.view.unifiedui.theme.survey.SurveyTheme
 
 internal data class UnifiedTheme(
@@ -13,5 +14,6 @@ internal data class UnifiedTheme(
     val callTheme: CallTheme? = null,
     val chatTheme: ChatTheme? = null,
     val surveyTheme: SurveyTheme? = null,
-    val callVisualizerTheme: CallVisualizerTheme? = null
+    val callVisualizerTheme: CallVisualizerTheme? = null,
+    val secureConversationsWelcomeScreenTheme: SecureConversationsWelcomeScreenTheme? = null
 )

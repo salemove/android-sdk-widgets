21ca1edcde251fd29e74b72c3f1e878048340337
Aleksandr Chatsky <aleksandr.chatsky@glia.com>
2023-03-09 15:28:57 +0200

    Add global listener for Call Visualizer engagement start event
    MOB-1934

--- widgetssdk/src/main/java/com/glia/widgets/core/callvisualizer/CallVisualizerManager.java
+++ widgetssdk/src/main/java/com/glia/widgets/core/callvisualizer/CallVisualizerManager.java
@@ -2,37 +2,65 @@ package com.glia.widgets.core.callvisualizer;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
+
 import com.glia.widgets.callvisualizer.CallVisualizerRepository;
 import com.glia.widgets.core.callvisualizer.domain.VisitorCodeViewBuilderUseCase;
 import com.glia.widgets.core.callvisualizer.domain.CallVisualizer;
+import com.glia.widgets.core.engagement.GliaEngagementRepository;
 import com.glia.widgets.di.Dependencies;
 import com.glia.widgets.view.VisitorCodeView;
 
 public class CallVisualizerManager implements CallVisualizer {
 
-    private CallVisualizerRepository repository;
+    private final CallVisualizerRepository callVisualizerRepository;
     private final VisitorCodeViewBuilderUseCase buildVisitorCodeUseCase;
+    private final GliaEngagementRepository engagementRepository;
+    private Runnable onEngagementStartRunnable;
+    private boolean isListeningForEngagement = false;
 
     public CallVisualizerManager(
             VisitorCodeViewBuilderUseCase buildVisitorCodeUseCase,
-            CallVisualizerRepository repository
+            CallVisualizerRepository callVisualizerRepository,
+            GliaEngagementRepository engagementRepository
     ) {
         this.buildVisitorCodeUseCase = buildVisitorCodeUseCase;
-        this.repository = repository;
+        this.callVisualizerRepository = callVisualizerRepository;
+        this.engagementRepository = engagementRepository;
     }
 
     @Override
     public VisitorCodeView createVisitorCodeView(Context context) {
+        startListeningForEngagements();
         return buildVisitorCodeUseCase.invoke(context, false);
     }
 
     @Override
     public void showVisitorCodeDialog(Context context) {
+        startListeningForEngagements();
         Dependencies.getControllerFactory().getDialogController().showVisitorCodeDialog();
     }
 
     @Override
     public void addVisitorContext(String visitorContext) {
-        repository.addVisitorContext(visitorContext);
+        callVisualizerRepository.addVisitorContext(visitorContext);
+    }
+
+    @Override
+    public void onEngagementStart(@NonNull Runnable runnable) {
+        onEngagementStartRunnable = runnable;
+    }
+
+    private void startListeningForEngagements() {
+        // Can't start Core SDK listener on this class init/construction because this class is initialized
+        // on `Application.onCreate()` before the Glia Core SDK is initialised.
+        if (isListeningForEngagement) return;
+        isListeningForEngagement = true;
+        // Subscribe only once because otherwise need to add method to unsubscribe
+        engagementRepository.listenForCallVisualizerEngagement((engagement) -> {
+            if (onEngagementStartRunnable != null) {
+                onEngagementStartRunnable.run();
+            }
+        });
     }
 }

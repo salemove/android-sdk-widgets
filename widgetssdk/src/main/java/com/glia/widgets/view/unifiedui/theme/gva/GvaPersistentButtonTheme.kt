f6be3b8d1724774eca32a7fa81f832901307248a
Andrii Horishnii <andrii.horishnii@masterofcode.com>
2023-07-19 23:03:58 +0300

    GVA: Apply Unified Customization to the Persistent Button card
    MOB-2373

---
+++ widgetssdk/src/main/java/com/glia/widgets/view/unifiedui/theme/gva/GvaPersistentButtonTheme.kt
@@ -0,0 +1,11 @@
+package com.glia.widgets.view.unifiedui.theme.gva
+
+import com.glia.widgets.view.unifiedui.theme.base.ButtonTheme
+import com.glia.widgets.view.unifiedui.theme.base.LayerTheme
+import com.glia.widgets.view.unifiedui.theme.base.TextTheme
+
+internal data class GvaPersistentButtonTheme(
+    val title: TextTheme? = null,
+    val background: LayerTheme? = null,
+    val button: ButtonTheme? = null
+)

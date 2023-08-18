58a479c0685b083445419586795c7472f547dda9
Davit Dolmazyan <davit.dolmazyan@glia.com>
2023-08-18 17:35:13 +0300

    Devs want deep link support in Widgets SDK
    - `glia://widgets/settings` - should route to the settings page.
    
    MOB 2551

--- app/src/main/java/com/glia/exampleapp/Activity.java
+++ app/src/main/java/com/glia/exampleapp/Activity.java
@@ -1,9 +1,11 @@
 package com.glia.exampleapp;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
 
 import com.glia.androidsdk.Glia;
 import com.glia.widgets.GliaWidgets;
@@ -23,5 +25,11 @@ public class Activity extends AppCompatActivity {
         if (!Glia.isInitialized() && uri != null) {
             GliaWidgets.init(GliaWidgetsConfigManager.obtainConfigFromDeepLink(uri, getApplicationContext()));
         }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Navigation.findNavController(this, R.id.nav_host_fragment).handleDeepLink(intent);
     }
 }

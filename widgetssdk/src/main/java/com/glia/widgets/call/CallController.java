de09d001534479387fd01e731cc0da6dd7c439d3
Andriy Shevtsov <andriy.shevtsov@masterofcode.com>
2023-08-28 19:25:23 +0300

    Do not unsubscribe from engagement events for Call Visualizer
    Some listeners subscribe only once, so we shouldn't unsubscribe
    
    MOB-2556

--- widgetssdk/src/main/java/com/glia/widgets/call/CallController.java
+++ widgetssdk/src/main/java/com/glia/widgets/call/CallController.java
@@ -328,7 +328,7 @@ public class CallController implements
         viewCallback = null;
 
         if (!retain) {
-            disposable.dispose();
+            disposable.clear();
             mediaUpgradeOfferRepository.stopAll();
             mediaUpgradeOfferRepositoryCallback = null;
             if (callTimerStatusListener != null) {
@@ -823,10 +823,7 @@ public class CallController implements
                         break;
                 }
             });
-            omnibrowseEngagement.on(Engagement.Events.END, engagementState -> {
-                Glia.omnibrowse.off(Omnibrowse.Events.ENGAGEMENT);
-                viewCallback.destroyView();
-            });
+            omnibrowseEngagement.on(Engagement.Events.END, engagementState -> viewCallback.destroyView());
         });
     }
 

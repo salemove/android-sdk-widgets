a41b6a8390db681c99589eed4cc70c4c1c3c0756
Davit Dolmazyan <davit.dolmazyan@glia.com>
2023-10-11 12:56:24 +0300

    Refactor Opt-in dialog strings to support Custom Locales
    MOB 2722

--- widgetssdk/src/main/java/com/glia/widgets/StringProvider.java
+++ widgetssdk/src/main/java/com/glia/widgets/StringProvider.java
@@ -3,5 +3,5 @@ package com.glia.widgets;
 import androidx.annotation.StringRes;
 
 public interface StringProvider {
-    public String getRemoteString(@StringRes int stringKey, StringKeyPair... values);
+    String getRemoteString(@StringRes int stringKey, StringKeyPair... values);
 }

6d2866ab5229bca2efbd5da58f4371d157c8ef28
Karl Valliste <karl.valliste@gmail.com>
2023-10-24 12:28:26 +0300

    Implemented manual locale override in widgets
    MOB-2759

--- widgetssdk/src/androidTest/java/com/glia/widgets/AndroidTestStringProvider.kt
+++ widgetssdk/src/androidTest/java/com/glia/widgets/AndroidTestStringProvider.kt
@@ -2,7 +2,7 @@ package com.glia.widgets
 
 import android.content.Context
 
-class AndroidTestStringProvider(private val context: Context): StringProvider {
+class AndroidTestStringProvider(private val context: Context) : StringProvider {
 
     override fun getRemoteString(stringKey: Int, vararg values: StringKeyPair?): String {
         val vals = values.map { pair -> pair?.value }.toTypedArray()

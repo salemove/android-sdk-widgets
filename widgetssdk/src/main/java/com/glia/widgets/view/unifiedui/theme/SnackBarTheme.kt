d37f630efffd9828679e7b6584314bf12b642065
Davit Dolmazyan <davit.dolmazyan@glia.com>
2023-10-24 16:25:39 +0300

    Add Unified UI Support for LO indicator
    MOB 2785

---
+++ widgetssdk/src/main/java/com/glia/widgets/view/unifiedui/theme/SnackBarTheme.kt
@@ -0,0 +1,8 @@
+package com.glia.widgets.view.unifiedui.theme
+
+import com.glia.widgets.view.unifiedui.theme.base.ColorTheme
+
+internal data class SnackBarTheme(
+    val backgroundColorTheme: ColorTheme?,
+    val textColorTheme: ColorTheme?
+)

60e4b6f4e4bc8caf7b0f08f8420a2771f4f9d909
Andriy Shevtsov <andriy.shevtsov@masterofcode.com>
2023-10-30 19:25:10 +0200

    Send logs through Core SDK logger
    MOB-2789

--- widgetssdk/src/main/java/com/glia/widgets/helper/Logger.kt
+++ widgetssdk/src/main/java/com/glia/widgets/helper/Logger.kt
@@ -1,23 +1,88 @@
 package com.glia.widgets.helper
 
 import android.util.Log
+import com.glia.androidsdk.Glia
 import com.glia.widgets.BuildConfig
+import java.lang.reflect.InvocationTargetException
+import java.lang.reflect.Method
 
 object Logger {
+
+    private var coreSdkLogger: com.glia.androidsdk.internal.logger.Logger? = null
+    private var coreSdkLogDFunction: Method? = null
+    private var coreSDKLogEFunction: Method? = null
+
+    @Suppress("unused")
+    @JvmStatic
+    fun setCoreSdkLogger(coreSdkLogger: com.glia.androidsdk.internal.logger.Logger) {
+        d(TAG, "Core SDK logger set successfully")
+        this.coreSdkLogger = coreSdkLogger
+    }
+
+    @JvmStatic
+    fun getCoreSdkLogger() : com.glia.androidsdk.internal.logger.Logger? = coreSdkLogger
+
     @JvmStatic
     fun d(tag: String, message: String?) {
         if (BuildConfig.DEBUG) {
             // No need to log an empty message
-            Log.d(tag, message ?: return)
+            logDToCoreSdk(tag, message ?: return)
         }
     }
 
     @JvmStatic
     fun e(tag: String, message: String?) {
         if (BuildConfig.DEBUG) {
-            // //No need to log an empty message
-            Log.e(tag, message ?: return)
+            //No need to log an empty message
+            logEToCoreSdk(tag, message ?: return)
         }
+    }
+
+    private fun logDToCoreSdk(tag: String, message: String?) {
+        if (!Glia.isInitialized()) return
+        if (message == null) return
+
+        if (coreSdkLogDFunction == null) {
+            try {
+                coreSdkLogDFunction = getCoreSdkLogger()?.javaClass?.getDeclaredMethod(
+                    "d",
+                    String::class.java,
+                    String::class.java
+                )
+            } catch (e: IllegalAccessException) {
+                Log.d(TAG, "Unable to log message using Core SDK logger $e")
+            } catch (e: InvocationTargetException) {
+                Log.d(TAG, "Unable to log message using Core SDK logger $e")
+            } catch (e: NoSuchMethodException) {
+                Log.d(TAG, "Unable to log message using Core SDK logger $e")
+            }
+        }
+
+        coreSdkLogDFunction?.invoke(null, tag, message)
+    }
+
+    private fun logEToCoreSdk(tag: String, message: String?, throwable: Throwable? = null) {
+        if (!Glia.isInitialized()) return
+        if (message == null) return
+
+        if (coreSDKLogEFunction == null) {
+            try {
+                coreSDKLogEFunction = getCoreSdkLogger()?.javaClass?.getDeclaredMethod(
+                    "e",
+                    String::class.java,
+                    String::class.java,
+                    Throwable::class.java
+                )
+            } catch (e: IllegalAccessException) {
+                Log.d(TAG, "Unable to log message using Core SDK logger $e")
+            } catch (e: InvocationTargetException) {
+                Log.d(TAG, "Unable to log message using Core SDK logger $e")
+            } catch (e: NoSuchMethodException) {
+                Log.d(TAG, "Unable to log message using Core SDK logger $e")
+            }
+        }
+
+        coreSDKLogEFunction?.invoke(null, tag, message, throwable)
     }
 
     @JvmStatic
@@ -25,7 +90,7 @@ object Logger {
         if (BuildConfig.DEBUG) {
             // No problem with lint in here
             if (message.isNullOrBlank() && tr == null) return
-            Log.e(tag, message, tr)
+            logEToCoreSdk(tag, message, tr)
         }
     }
 }

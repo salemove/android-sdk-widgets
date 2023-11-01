2481de76e430af83de98aae41931986bf63ce4d7
Aleksandr Chatsky <aleksandr.chatsky@glia.com>
2023-11-01 14:36:30 +0200

    Make confirmation dialog and LO toast message show by default
    MOB-2799

--- widgetssdk/src/main/java/com/glia/widgets/view/snackbar/ActivityWatcherForLiveObservationController.kt
+++ widgetssdk/src/main/java/com/glia/widgets/view/snackbar/ActivityWatcherForLiveObservationController.kt
@@ -3,14 +3,19 @@ package com.glia.widgets.view.snackbar
 import kotlin.properties.Delegates
 
 internal class ActivityWatcherForLiveObservationController(
-    private var liveObservationUseCase: LiveObservationUseCase
+    private var liveObservationUseCase: LiveObservationUseCase,
+    private val liveObservationPopupUseCase: LiveObservationPopupUseCase
 ) : ActivityWatcherForLiveObservationContract.Controller {
     private var liveObservationWatcher: ActivityWatcherForLiveObservationContract.Watcher by Delegates.notNull()
 
     override fun init() {
         liveObservationUseCase.init()
         liveObservationUseCase {
-            liveObservationWatcher.showSnackBar()
+            liveObservationPopupUseCase { shouldShow ->
+                if (shouldShow) {
+                    liveObservationWatcher.showSnackBar()
+                }
+            }
         }
     }
 

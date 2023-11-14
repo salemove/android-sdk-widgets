33d347cb3bc68bd69ec902a20e31edc657e4631f
Davit Dolmazyan <davit.dolmazyan@glia.com>
2023-11-14 10:30:33 +0200

    Snapshot test for Alert Dialog
    MOB 2805

--- widgetssdk/src/main/java/com/glia/widgets/view/unifiedui/theme/ThemeWrapper.kt
+++ widgetssdk/src/main/java/com/glia/widgets/view/unifiedui/theme/ThemeWrapper.kt
@@ -1,6 +1,7 @@
 package com.glia.widgets.view.unifiedui.theme
 
 import android.graphics.Typeface
+import androidx.annotation.DrawableRes
 import com.glia.widgets.view.unifiedui.theme.alert.AlertTheme
 
 internal interface TypefaceThemeWrapper<T : Any> {
@@ -8,5 +9,9 @@ internal interface TypefaceThemeWrapper<T : Any> {
     val typeface: Typeface?
 }
 
-internal data class AlertThemeWrapper(override val theme: AlertTheme, override val typeface: Typeface?, val whiteLabel: Boolean?) :
-    TypefaceThemeWrapper<AlertTheme>
+internal data class AlertThemeWrapper(
+    override val theme: AlertTheme,
+    override val typeface: Typeface?,
+    val whiteLabel: Boolean?,
+    @DrawableRes val iconLeaveQueue: Int?
+) : TypefaceThemeWrapper<AlertTheme>

dae88f288f579be98c3e88eff73bdab7c68ca368
Andrii Horishnii <andrii.horishnii@masterofcode.com>
2023-11-30 15:52:01 +0200

    Added snapshot tests for confirmation alert with link buttons
    MOB-2873

--- widgetssdk/src/testSnapshot/java/com/glia/widgets/dialog/ReversedOptionDialogTest.kt
+++ widgetssdk/src/testSnapshot/java/com/glia/widgets/dialog/ReversedOptionDialogTest.kt
@@ -1,11 +1,12 @@
 package com.glia.widgets.dialog
 
 import com.glia.widgets.SnapshotTest
+import com.glia.widgets.snapshotutils.SnapshotDialog
 import com.glia.widgets.view.dialog.base.DialogPayload
 import com.glia.widgets.view.dialog.base.DialogType
 import org.junit.Test
 
-class ReversedOptionDialogTest : SnapshotTest(), DialogData {
+class ReversedOptionDialogTest : SnapshotTest(), SnapshotDialog {
 
     private val dialogType: DialogType = DialogType.ReversedOption(
         DialogPayload.Option(

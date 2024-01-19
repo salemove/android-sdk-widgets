5a878bdc4fd8bc33250ad0ed719d4c50fef414f0
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-01-19 17:39:45 +0200

    Improve transitions btw Screens and minor UI fixes
    MOB 3002

--- widgetssdk/src/main/java/com/glia/widgets/view/dialog/holder/DialogHolderActivity.kt
+++ widgetssdk/src/main/java/com/glia/widgets/view/dialog/holder/DialogHolderActivity.kt
@@ -7,13 +7,14 @@ import androidx.appcompat.app.AppCompatActivity
 internal class DialogHolderActivity : AppCompatActivity() {
 
     override fun finish() {
-        super.finish()
         overridePendingTransition(0, 0)
+        super.finish()
     }
 
     companion object {
         fun start(activity: Activity) {
-            activity.startActivity(Intent(activity, DialogHolderActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
+            activity.startActivity(Intent(activity, DialogHolderActivity::class.java))
+            activity.overridePendingTransition(0, android.R.anim.fade_out)
         }
     }
 

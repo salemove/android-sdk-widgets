a1ff63ad755d26af28c6353e8ea2337d167d1c1c
Aleksandr Chatsky <aleksandr.chatsky@glia.com>
2024-01-26 12:59:30 +0200

    Make SimpleTextWatcher class internal
    Currently this class is public to integrators and is even used in out TestApp.
    
    GR-553

--- widgetssdk/src/main/java/com/glia/widgets/helper/SimpleTextWatcher.kt
+++ widgetssdk/src/main/java/com/glia/widgets/helper/SimpleTextWatcher.kt
@@ -3,7 +3,7 @@ package com.glia.widgets.helper
 import android.text.Editable
 import android.text.TextWatcher
 
-abstract class SimpleTextWatcher : TextWatcher {
+internal abstract class SimpleTextWatcher : TextWatcher {
     override fun beforeTextChanged(charSequence: CharSequence, start: Int, count: Int, after: Int) {}
 
     override fun onTextChanged(charSequence: CharSequence, start: Int, before: Int, count: Int) {}

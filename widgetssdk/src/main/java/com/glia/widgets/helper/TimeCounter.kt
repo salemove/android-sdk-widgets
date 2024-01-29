e30fa57d7f66e690ec08d952539e45899a1f803a
Aleksandr Chatsky <aleksandr.chatsky@glia.com>
2024-01-29 10:22:58 +0200

    Converted couple of classes with very common names to internal
    Did this to prevent integrators accidentally using those classes because their name is very common in Android development. To make them internal had to also convert them to Kotlin
    
    GR-553

---
+++ widgetssdk/src/main/java/com/glia/widgets/helper/TimeCounter.kt
@@ -0,0 +1,100 @@
+package com.glia.widgets.helper
+
+import java.util.Timer
+import java.util.TimerTask
+
+internal class TimeCounter {
+    private val formattedListeners: MutableList<FormattedTimerStatusListener> = ArrayList()
+    private val rawListeners: MutableList<RawTimerStatusListener> = ArrayList()
+    private var timerTask: TimerTask? = null
+    private var timer: Timer? = null
+    fun startNew(timerDelayMs: Int, timerIntervalMs: Int) {
+        Logger.d(TAG, "startNew! timerDelay: " + Integer.valueOf(timerDelayMs).toString() +
+                    ", timerInterval: " + Integer.valueOf(timerIntervalMs).toString())
+        createNewTimerTask(timerDelayMs, timerIntervalMs)
+        createNewTimer()
+        timer?.schedule(timerTask, timerDelayMs.toLong(), timerIntervalMs.toLong())
+    }
+
+    fun stop() {
+        Logger.d(TAG, "stop")
+        timerTask?.cancel()
+        timerTask = null
+        timer?.cancel()
+        timer = null
+    }
+
+    fun clear() {
+        Logger.d(TAG, "clear")
+        stop()
+        formattedListeners.clear()
+        rawListeners.clear()
+    }
+
+    fun addFormattedValueListener(listener: FormattedTimerStatusListener) {
+        Logger.d(TAG, "addFormattedListener")
+        formattedListeners.add(listener)
+    }
+
+    fun removeFormattedValueListener(listener: FormattedTimerStatusListener) {
+        Logger.d(TAG, "removeFormattedListener")
+        formattedListeners.remove(listener)
+    }
+
+    fun addRawValueListener(listener: RawTimerStatusListener) {
+        Logger.d(TAG, "addRawListener")
+        rawListeners.add(listener)
+    }
+
+    private fun createNewTimer() {
+        timer?.cancel()
+        timer = Timer()
+    }
+
+    private fun createNewTimerTask(timerDelayMs: Int, timerIntervalMs: Int) {
+        timerTask?.cancel()
+        timerTask = object : TimerTask() {
+            var value = timerDelayMs
+
+            override fun run() {
+                Logger.d(TAG, "Timer value: $value")
+                for (listener in rawListeners) {
+                    listener.onNewRawTimerValue(value)
+                }
+
+                if (formattedListeners.isNotEmpty()) {
+                    val time = formatElapsedTime(value.toLong())
+                    Logger.d(TAG, "Formatted timer: $time")
+                    for (listener in formattedListeners) {
+                        listener.onNewFormattedTimerValue(time)
+                    }
+                }
+                value += timerIntervalMs
+            }
+
+            override fun cancel(): Boolean {
+                for (listener in rawListeners) {
+                    listener.onRawTimerCancelled()
+                }
+                for (listener in formattedListeners) {
+                    listener.onFormattedTimerCancelled()
+                }
+                Logger.d(TAG, "cancel")
+                return super.cancel()
+            }
+        }
+    }
+
+    val isRunning: Boolean
+        get() = timerTask != null
+
+    interface FormattedTimerStatusListener {
+        fun onNewFormattedTimerValue(formattedValue: String)
+        fun onFormattedTimerCancelled()
+    }
+
+    interface RawTimerStatusListener {
+        fun onNewRawTimerValue(timerValue: Int)
+        fun onRawTimerCancelled()
+    }
+}
--- widgetssdk/src/main/java/com/glia/widgets/helper/rx/Schedulers.java
+++
@@ -1,9 +0,0 @@
-package com.glia.widgets.helper.rx;
-
-import io.reactivex.Scheduler;
-
-public interface Schedulers {
-    Scheduler getComputationScheduler();
-
-    Scheduler getMainScheduler();
-}

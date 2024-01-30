48770bd5fbd466f79f7e396d04155e5a1073b834
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-01-30 15:33:52 +0200

    Remove resetting EngagementRepository from SetEngagementConfigUseCase
    MOB 3035

--- widgetssdk/src/main/java/com/glia/widgets/core/engagement/domain/SetEngagementConfigUseCase.kt
+++ widgetssdk/src/main/java/com/glia/widgets/core/engagement/domain/SetEngagementConfigUseCase.kt
@@ -2,19 +2,10 @@ package com.glia.widgets.core.engagement.domain
 
 import com.glia.widgets.chat.ChatType
 import com.glia.widgets.core.engagement.GliaEngagementConfigRepository
-import com.glia.widgets.engagement.EngagementRepository
 
-internal class SetEngagementConfigUseCase(
-    private val engagementConfigRepository: GliaEngagementConfigRepository,
-    private val engagementRepository: EngagementRepository
-) {
+internal class SetEngagementConfigUseCase(private val engagementConfigRepository: GliaEngagementConfigRepository) {
     operator fun invoke(chatType: ChatType, queueIds: Array<String>) {
         engagementConfigRepository.chatType = chatType
         engagementConfigRepository.queueIds = queueIds
-
-        // Resetting just in case there is a pending Survey
-        if (chatType == ChatType.SECURE_MESSAGING) {
-            engagementRepository.reset()
-        }
     }
 }

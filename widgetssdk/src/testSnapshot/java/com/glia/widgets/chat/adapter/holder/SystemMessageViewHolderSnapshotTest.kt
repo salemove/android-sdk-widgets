cc6bdcfa9fc0d28068dd040cf64b410d65afb174
Andrii Horishnii <andrii.horishnii@masterofcode.com>
2024-01-31 21:20:50 +0200

    Added snapshot tests for the System Message chat item
    MOB-3027

---
+++ widgetssdk/src/testSnapshot/java/com/glia/widgets/chat/adapter/holder/SystemMessageViewHolderSnapshotTest.kt
@@ -0,0 +1,81 @@
+package com.glia.widgets.chat.adapter.holder
+
+import com.glia.widgets.SnapshotTest
+import com.glia.widgets.UiTheme
+import com.glia.widgets.databinding.ChatReceiveMessageContentBinding
+import com.glia.widgets.di.Dependencies
+import com.glia.widgets.snapshotutils.SnapshotChatScreen
+import com.glia.widgets.snapshotutils.SnapshotProviders
+import com.glia.widgets.snapshotutils.SnapshotStrings
+import com.glia.widgets.view.unifiedui.theme.UnifiedTheme
+import org.junit.Test
+
+class SystemMessageViewHolderSnapshotTest : SnapshotTest(), SnapshotChatScreen, SnapshotProviders, SnapshotStrings {
+
+    // MARK: Tests
+
+    @Test
+    fun defaultView() {
+        snapshot(
+            setupView().itemView
+        )
+    }
+
+    @Test
+    fun withUiTheme() {
+        snapshot(
+            setupView(
+                uiTheme = uiTheme()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun withGlobalColors() {
+        snapshot(
+            setupView(
+                unifiedTheme = unifiedThemeWithGlobalColors()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun withUnifiedTheme() {
+        snapshot(
+            setupView(
+                unifiedTheme = unifiedTheme()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun withUnifiedThemeWithoutOperatorMessage() {
+        snapshot(
+            setupView(
+                unifiedTheme = unifiedThemeWithoutOperatorMessage()
+            ).itemView
+        )
+    }
+
+    // MARK: utils for tests
+
+    private fun setupView(
+        message: String = mediumLengthTexts().joinToString(separator = " "),
+        unifiedTheme: UnifiedTheme? = null,
+        uiTheme: UiTheme = UiTheme()
+    ): SystemMessageViewHolder {
+        stringProviderMock()
+        unifiedTheme?.let { Dependencies.getGliaThemeManager().theme = it }
+
+        setOnEndListener {
+            Dependencies.getGliaThemeManager().theme = null
+        }
+
+        return SystemMessageViewHolder(
+            ChatReceiveMessageContentBinding.inflate(layoutInflater),
+            uiTheme
+        ).also { viewHolder ->
+            viewHolder.bind(message)
+        }
+    }
+}

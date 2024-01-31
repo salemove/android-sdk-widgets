e5a76d2dbbf9ab2968d7f193e2218c10af23025e
Andrii Horishnii <andrii.horishnii@masterofcode.com>
2024-01-31 11:41:32 +0200

    Added snapshot tests for the Operator File Attachment chat item
    MOB-3023

---
+++ widgetssdk/src/testSnapshot/java/com/glia/widgets/chat/adapter/holder/OperatorFileAttachmentViewHolderSnapshotTest.kt
@@ -0,0 +1,210 @@
+package com.glia.widgets.chat.adapter.holder
+
+import android.widget.LinearLayout
+import com.glia.androidsdk.chat.AttachmentFile
+import com.glia.widgets.R
+import com.glia.widgets.SnapshotTest
+import com.glia.widgets.UiTheme
+import com.glia.widgets.chat.adapter.ChatAdapter
+import com.glia.widgets.chat.adapter.holder.fileattachment.OperatorFileAttachmentViewHolder
+import com.glia.widgets.chat.model.OperatorAttachmentItem
+import com.glia.widgets.di.Dependencies
+import com.glia.widgets.snapshotutils.SnapshotAttachment
+import com.glia.widgets.snapshotutils.SnapshotChatScreen
+import com.glia.widgets.snapshotutils.SnapshotPicasso
+import com.glia.widgets.snapshotutils.SnapshotProviders
+import com.glia.widgets.view.unifiedui.theme.UnifiedTheme
+import org.junit.Test
+
+class OperatorFileAttachmentViewHolderSnapshotTest : SnapshotTest(
+    renderingMode = fullWidthRenderMode
+), SnapshotChatScreen, SnapshotAttachment, SnapshotProviders, SnapshotPicasso {
+
+    // MARK: without header
+
+    @Test
+    fun withoutHeader() {
+        snapshot(
+            setupView(
+                operatorAttachmentItemFile()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun withoutHeaderWithUiTheme() {
+        snapshot(
+            setupView(
+                operatorAttachmentItemFile(),
+                uiTheme = uiTheme()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun withoutHeaderWithGlobalColors() {
+        snapshot(
+            setupView(
+                operatorAttachmentItemFile(),
+                unifiedTheme = unifiedThemeWithGlobalColors()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun withoutHeaderWithUnifiedTheme() {
+        snapshot(
+            setupView(
+                operatorAttachmentItemFile(),
+                unifiedTheme = unifiedTheme()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun withoutHeaderWithUnifiedThemeWithoutOperatorMessage() {
+        snapshot(
+            setupView(
+                operatorAttachmentItemFile(),
+                unifiedTheme = unifiedThemeWithoutOperatorMessage()
+            ).itemView
+        )
+    }
+
+    // MARK: with header
+
+    @Test
+    fun withHeader() {
+        snapshot(
+            setupView(
+                operatorAttachmentItemFile(showChatHead = true)
+            ).itemView
+        )
+    }
+
+    @Test
+    fun withHeaderWithUiTheme() {
+        snapshot(
+            setupView(
+                operatorAttachmentItemFile(showChatHead = true),
+                uiTheme = uiTheme()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun withHeaderWithGlobalColors() {
+        snapshot(
+            setupView(
+                operatorAttachmentItemFile(showChatHead = true),
+                unifiedTheme = unifiedThemeWithGlobalColors()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun withHeaderWithUnifiedTheme() {
+        snapshot(
+            setupView(
+                operatorAttachmentItemFile(showChatHead = true),
+                unifiedTheme = unifiedTheme()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun withHeaderWithUnifiedThemeWithoutOperatorMessage() {
+        snapshot(
+            setupView(
+                operatorAttachmentItemFile(showChatHead = true),
+                unifiedTheme = unifiedThemeWithoutOperatorMessage()
+            ).itemView
+        )
+    }
+
+    // MARK: with operator header image
+
+    private fun operatorImageHeaderItem() = operatorAttachmentItemFile(
+        showChatHead = true,
+        operatorProfileImgUrl = "https://worldwide-operators/best-profile-image"
+    )
+
+    @Test
+    fun operatorImageHeader() {
+        snapshot(
+            setupView(
+                operatorImageHeaderItem()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun operatorImageHeaderWithUiTheme() {
+        snapshot(
+            setupView(
+                operatorImageHeaderItem(),
+                uiTheme = uiTheme()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun operatorImageHeaderWithGlobalColors() {
+        snapshot(
+            setupView(
+                operatorImageHeaderItem(),
+                unifiedTheme = unifiedThemeWithGlobalColors()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun operatorImageHeaderWithUnifiedTheme() {
+        snapshot(
+            setupView(
+                operatorImageHeaderItem(),
+                unifiedTheme = unifiedTheme()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun operatorImageHeaderWithUnifiedThemeWithoutOperatorMessage() {
+        snapshot(
+            setupView(
+                operatorImageHeaderItem(),
+                unifiedTheme = unifiedThemeWithoutOperatorMessage()
+            ).itemView
+        )
+    }
+
+    // MARK: utils for tests
+
+    private fun setupView(
+        item: OperatorAttachmentItem.File,
+        unifiedTheme: UnifiedTheme? = null,
+        uiTheme: UiTheme = UiTheme()
+    ): OperatorFileAttachmentViewHolder {
+        stringProviderMock()
+        picassoMock(listOf(R.drawable.test_launcher2))
+        unifiedTheme?.let { Dependencies.getGliaThemeManager().theme = it }
+
+        setOnEndListener {
+            Dependencies.getGliaThemeManager().theme = null
+        }
+
+        return OperatorFileAttachmentViewHolder(
+            layoutInflater.inflate(
+                R.layout.chat_attachment_operator_file_layout,
+                LinearLayout(context),
+                false
+            ),
+            uiTheme
+        ).also { viewHolder ->
+            viewHolder.bind(item, object : ChatAdapter.OnFileItemClickListener {
+                override fun onFileOpenClick(file: AttachmentFile) {}
+                override fun onFileDownloadClick(file: AttachmentFile) {}
+            })
+        }
+    }
+}

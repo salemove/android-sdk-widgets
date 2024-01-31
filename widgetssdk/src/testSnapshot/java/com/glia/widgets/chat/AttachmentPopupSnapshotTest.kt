3bec6d8063a12dc818fb460f7ad0b720ed5628e7
Andrii Horishnii <andrii.horishnii@masterofcode.com>
2024-01-31 23:16:19 +0200

    Added snapshot tests for the Add Attachment popup
    MOB-3020

---
+++ widgetssdk/src/testSnapshot/java/com/glia/widgets/chat/AttachmentPopupSnapshotTest.kt
@@ -0,0 +1,70 @@
+package com.glia.widgets.chat
+
+import android.view.View
+import android.widget.LinearLayout
+import com.glia.widgets.SnapshotTest
+import com.glia.widgets.snapshotutils.SnapshotProviders
+import com.glia.widgets.snapshotutils.SnapshotTheme
+import com.glia.widgets.view.unifiedui.theme.chat.AttachmentsPopupTheme
+import org.junit.Test
+import org.mockito.kotlin.mock
+
+class AttachmentPopupSnapshotTest : SnapshotTest(), SnapshotTheme, SnapshotProviders {
+
+    // MARK: Tests
+
+    @Test
+    fun defaultTheme() {
+        setupView {
+            snapshot(it)
+        }
+    }
+
+    @Test
+    fun withGlobalColors() {
+        setupView(
+            unifiedThemeWithGlobalColors().chatTheme?.attachmentsPopup
+        ) {
+            snapshot(it)
+        }
+    }
+
+    @Test
+    fun chatWithUnifiedTheme() {
+        setupView(
+            unifiedTheme().chatTheme?.attachmentsPopup
+        ) {
+            snapshot(it)
+        }
+    }
+
+    @Test
+    fun secureConversationsWithUnifiedTheme() {
+        setupView(
+            unifiedTheme().secureConversationsWelcomeScreenTheme?.pickMediaTheme
+        ) {
+            snapshot(it)
+        }
+    }
+
+    // MARK: utils for tests
+
+    private fun setupView(
+        unifiedTheme: AttachmentsPopupTheme? = null,
+        viewCallback: (View) -> Unit
+    ) : AttachmentPopup {
+        stringProviderMock()
+
+        val anchor = LinearLayout(context)
+        return AttachmentPopup(
+            anchor,
+            unifiedTheme
+        ) {
+            it.layoutParams = LinearLayout.LayoutParams(650, LinearLayout.LayoutParams.WRAP_CONTENT)
+            viewCallback(it)
+            mock()
+        }.apply {
+            show(anchor, mock(), mock(), mock())
+        }
+    }
+}

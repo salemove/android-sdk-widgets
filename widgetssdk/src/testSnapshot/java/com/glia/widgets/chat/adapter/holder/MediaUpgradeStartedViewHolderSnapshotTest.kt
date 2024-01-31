8f90d40a54b7da78600956959c0eff60e37a14b7
Andrii Horishnii <andrii.horishnii@masterofcode.com>
2024-01-31 20:43:13 +0200

    Added snapshot tests for the Media Upgrade Started chat item
    MOB-3025

---
+++ widgetssdk/src/testSnapshot/java/com/glia/widgets/chat/adapter/holder/MediaUpgradeStartedViewHolderSnapshotTest.kt
@@ -0,0 +1,154 @@
+package com.glia.widgets.chat.adapter.holder
+
+import com.glia.widgets.R
+import com.glia.widgets.SnapshotTest
+import com.glia.widgets.UiTheme
+import com.glia.widgets.chat.model.MediaUpgradeStartedTimerItem
+import com.glia.widgets.databinding.ChatMediaUpgradeLayoutBinding
+import com.glia.widgets.di.Dependencies
+import com.glia.widgets.snapshotutils.SnapshotChatScreen
+import com.glia.widgets.snapshotutils.SnapshotProviders
+import com.glia.widgets.view.unifiedui.theme.UnifiedTheme
+import com.google.gson.JsonObject
+import org.junit.Test
+
+class MediaUpgradeStartedViewHolderSnapshotTest : SnapshotTest(), SnapshotChatScreen, SnapshotProviders {
+
+    // MARK: Audio
+
+    @Test
+    fun audio() {
+        snapshot(
+            setupView(MediaUpgradeStartedTimerItem.Audio()).itemView
+        )
+    }
+
+    @Test
+    fun audioUiTheme() {
+        snapshot(
+            setupView(
+                MediaUpgradeStartedTimerItem.Audio(),
+                uiTheme = uiTheme()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun audioWithGlobalColors() {
+        snapshot(
+            setupView(
+                MediaUpgradeStartedTimerItem.Audio(),
+                unifiedTheme = unifiedThemeWithGlobalColors()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun audioWithUnifiedTheme() {
+        snapshot(
+            setupView(
+                MediaUpgradeStartedTimerItem.Audio(),
+                unifiedTheme = unifiedTheme()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun audioWithUnifiedThemeWithoutAudioUpgrade() {
+        snapshot(
+            setupView(
+                MediaUpgradeStartedTimerItem.Audio(),
+                unifiedTheme = unifiedThemeWithoutAudioUpgrade()
+            ).itemView
+        )
+    }
+
+    // MARK: Video
+
+    @Test
+    fun video() {
+        snapshot(
+            setupView(MediaUpgradeStartedTimerItem.Video()).itemView
+        )
+    }
+
+    @Test
+    fun videoUiTheme() {
+        snapshot(
+            setupView(
+                MediaUpgradeStartedTimerItem.Video(),
+                uiTheme = uiTheme()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun videoWithGlobalColors() {
+        snapshot(
+            setupView(
+                MediaUpgradeStartedTimerItem.Video(),
+                unifiedTheme = unifiedThemeWithGlobalColors()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun videoWithUnifiedTheme() {
+        snapshot(
+            setupView(
+                MediaUpgradeStartedTimerItem.Video(),
+                unifiedTheme = unifiedTheme()
+            ).itemView
+        )
+    }
+
+    @Test
+    fun videoWithUnifiedThemeWithoutVideoUpgrade() {
+        snapshot(
+            setupView(
+                MediaUpgradeStartedTimerItem.Video(),
+                unifiedTheme = unifiedThemeWithoutVideoUpgrade()
+            ).itemView
+        )
+    }
+
+    // MARK: utils for tests
+
+    private fun setupView(
+        item: MediaUpgradeStartedTimerItem,
+        unifiedTheme: UnifiedTheme? = null,
+        uiTheme: UiTheme = UiTheme(iconChatVideoUpgrade = R.drawable.ic_baseline_videocam)
+    ): MediaUpgradeStartedViewHolder {
+        stringProviderMock()
+        unifiedTheme?.let { Dependencies.getGliaThemeManager().theme = it }
+
+        setOnEndListener {
+            Dependencies.getGliaThemeManager().theme = null
+        }
+
+        return MediaUpgradeStartedViewHolder(
+            ChatMediaUpgradeLayoutBinding.inflate(layoutInflater),
+            uiTheme
+        ).also { viewHolder ->
+            viewHolder.bind(item)
+        }
+    }
+
+    private fun unifiedThemeWithoutAudioUpgrade(): UnifiedTheme = unifiedTheme(R.raw.test_unified_config) { unifiedTheme ->
+        unifiedTheme.add(
+            "chatScreen",
+            (unifiedTheme.remove("chatScreen") as JsonObject).also {
+                it.remove("audioUpgrade")
+            }
+        )
+    }
+
+    private fun unifiedThemeWithoutVideoUpgrade(): UnifiedTheme = unifiedTheme(R.raw.test_unified_config) { unifiedTheme ->
+        unifiedTheme.add(
+            "chatScreen",
+            (unifiedTheme.remove("chatScreen") as JsonObject).also {
+                it.remove("videoUpgrade")
+            }
+        )
+    }
+}

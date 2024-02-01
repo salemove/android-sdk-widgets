408485ed19507eb56d40b215ce32021b93fdc442
Andrii Horishnii <andrii.horishnii@masterofcode.com>
2024-02-01 23:14:48 +0200

    Added snapshot tests for WebBrowserView
    MOB-3049

---
+++ widgetssdk/src/testSnapshot/java/com/glia/widgets/webbrowser/WebBrowserViewSnapshotTest.kt
@@ -0,0 +1,68 @@
+package com.glia.widgets.webbrowser
+
+import com.android.ide.common.rendering.api.SessionParams
+import com.glia.widgets.R
+import com.glia.widgets.SnapshotTest
+import com.glia.widgets.di.Dependencies
+import com.glia.widgets.snapshotutils.SnapshotProviders
+import com.glia.widgets.view.unifiedui.theme.UnifiedTheme
+import org.junit.Test
+
+class WebBrowserViewSnapshotTest : SnapshotTest(
+    renderingMode = SessionParams.RenderingMode.FULL_EXPAND
+), SnapshotProviders {
+
+    // MARK: tests
+
+    @Test
+    fun defaultTheme() {
+        snapshot(
+            setupView()
+        )
+    }
+
+    @Test
+    fun defaultThemeWithGlobalColors() {
+        snapshot(
+            setupView(unifiedTheme = unifiedThemeWithGlobalColors())
+        )
+    }
+
+    @Test
+    fun defaultThemeWithUnifiedTheme() {
+        snapshot(
+            setupView(unifiedTheme = unifiedTheme())
+        )
+    }
+
+    @Test
+    fun defaultThemeWithUnifiedThemeWithoutWebBrowser() {
+        snapshot(
+            setupView(unifiedTheme = unifiedThemeWithoutWebBrowser())
+        )
+    }
+
+    // MARK: utils for tests
+
+    private fun setupView(
+        title: String = "Privacy policy",
+        unifiedTheme: UnifiedTheme? = null
+    ): WebBrowserView {
+        stringProviderMock()
+        resourceProviderMock()
+
+        unifiedTheme?.let { Dependencies.getGliaThemeManager().theme = it }
+
+        setOnEndListener {
+            Dependencies.getGliaThemeManager().theme = null
+        }
+
+        return WebBrowserView(context).also {
+            it.setTitle(title)
+        }
+    }
+
+    private fun unifiedThemeWithoutWebBrowser(): UnifiedTheme = unifiedTheme(R.raw.test_unified_config) { unifiedTheme ->
+        unifiedTheme.remove("webBrowserScreen")
+    }
+}

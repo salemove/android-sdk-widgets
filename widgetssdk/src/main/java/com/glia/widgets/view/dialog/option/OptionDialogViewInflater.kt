83da3c7b15fd8c0f2029f22be1d029293a4deef9
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-02-13 16:30:56 +0200

    Fix Dialogs don't apply custom icons

--- widgetssdk/src/main/java/com/glia/widgets/view/dialog/option/OptionDialogViewInflater.kt
+++ widgetssdk/src/main/java/com/glia/widgets/view/dialog/option/OptionDialogViewInflater.kt
@@ -4,85 +4,93 @@ import android.view.LayoutInflater
 import androidx.core.view.isGone
 import com.glia.widgets.view.dialog.base.DialogPayload
 import com.glia.widgets.view.dialog.base.DialogViewInflater
-import com.glia.widgets.view.unifiedui.theme.AlertThemeWrapper
+import com.glia.widgets.view.unifiedui.theme.AlertDialogConfiguration
 
 internal abstract class BaseOptionDialogViewInflater<T : BaseOptionDialogViewBinding<*>>(
     binding: T,
-    themeWrapper: AlertThemeWrapper,
+    themeWrapper: AlertDialogConfiguration,
     payload: DialogPayload.Option
 ) : DialogViewInflater<T, DialogPayload.Option>(binding, themeWrapper, payload) {
-    override fun setup(binding: T, themeWrapper: AlertThemeWrapper, payload: DialogPayload.Option) {
-        val theme = themeWrapper.theme
+    override fun setup(binding: T, configuration: AlertDialogConfiguration, payload: DialogPayload.Option) {
+        val theme = configuration.theme
 
-        binding.logoContainer.isGone = themeWrapper.whiteLabel ?: false
+        binding.logoContainer.isGone = configuration.properties.whiteLabel ?: false
         binding.poweredByTv.text = payload.poweredByText
 
-        setupText(binding.messageTv, payload.message, theme.message, themeWrapper.typeface)
+        setupText(binding.messageTv, payload.message, theme.message, configuration.properties.typeface)
     }
 }
 
 internal open class DefaultOptionDialogViewInflater<T : DefaultOptionDialogViewBinding<*>>(
     binding: T,
-    themeWrapper: AlertThemeWrapper,
+    themeWrapper: AlertDialogConfiguration,
     payload: DialogPayload.Option
 ) : BaseOptionDialogViewInflater<T>(binding, themeWrapper, payload) {
-    final override fun setup(binding: T, themeWrapper: AlertThemeWrapper, payload: DialogPayload.Option) {
-        super.setup(binding, themeWrapper, payload)
-        val theme = themeWrapper.theme
+    final override fun setup(binding: T, configuration: AlertDialogConfiguration, payload: DialogPayload.Option) {
+        super.setup(binding, configuration, payload)
+        val theme = configuration.theme
         setupButton(
             binding.positiveButton,
             payload.positiveButtonText,
             theme.positiveButton,
-            themeWrapper.typeface,
+            configuration.properties.typeface,
             payload.positiveButtonClickListener
         )
         setupButton(
             binding.negativeButton,
             payload.negativeButtonText,
             theme.negativeButton,
-            themeWrapper.typeface,
+            configuration.properties.typeface,
             payload.negativeButtonClickListener
         )
     }
 }
 
-internal class OptionDialogViewInflater(layoutInflater: LayoutInflater, themeWrapper: AlertThemeWrapper, payload: DialogPayload.Option) :
+internal class OptionDialogViewInflater(layoutInflater: LayoutInflater, themeWrapper: AlertDialogConfiguration, payload: DialogPayload.Option) :
     DefaultOptionDialogViewInflater<OptionDialogViewBinding>(OptionDialogViewBinding(layoutInflater), themeWrapper, payload)
 
-internal class VerticalOptionDialogViewInflater(layoutInflater: LayoutInflater, themeWrapper: AlertThemeWrapper, payload: DialogPayload.Option) :
+internal class VerticalOptionDialogViewInflater(
+    layoutInflater: LayoutInflater,
+    themeWrapper: AlertDialogConfiguration,
+    payload: DialogPayload.Option
+) :
     DefaultOptionDialogViewInflater<VerticalOptionDialogViewBinding>(VerticalOptionDialogViewBinding(layoutInflater), themeWrapper, payload)
 
 internal open class DefaultReversedOptionDialogViewInflater<T : DefaultReversedOptionDialogViewBinding<*>>(
     binding: T,
-    themeWrapper: AlertThemeWrapper,
+    themeWrapper: AlertDialogConfiguration,
     payload: DialogPayload.Option
 ) : BaseOptionDialogViewInflater<T>(binding, themeWrapper, payload) {
-    final override fun setup(binding: T, themeWrapper: AlertThemeWrapper, payload: DialogPayload.Option) {
-        super.setup(binding, themeWrapper, payload)
-        val theme = themeWrapper.theme
+    final override fun setup(binding: T, configuration: AlertDialogConfiguration, payload: DialogPayload.Option) {
+        super.setup(binding, configuration, payload)
+        val theme = configuration.theme
         setupButton(
             binding.positiveButton,
             payload.positiveButtonText,
             theme.negativeButton,// Since buttons are reversed, the positive button is actually GliaNegativeButton and should use NegativeButton theming
-            themeWrapper.typeface,
+            configuration.properties.typeface,
             payload.positiveButtonClickListener
         )
         setupButton(
             binding.negativeButton,
             payload.negativeButtonText,
             theme.positiveButton,// Since buttons are reversed, the positive button is actually GliaNegativeButton and should use NegativeButton theming
-            themeWrapper.typeface,
+            configuration.properties.typeface,
             payload.negativeButtonClickListener
         )
     }
 }
 
-internal class ReversedOptionDialogViewInflater(layoutInflater: LayoutInflater, themeWrapper: AlertThemeWrapper, payload: DialogPayload.Option) :
+internal class ReversedOptionDialogViewInflater(
+    layoutInflater: LayoutInflater,
+    themeWrapper: AlertDialogConfiguration,
+    payload: DialogPayload.Option
+) :
     DefaultReversedOptionDialogViewInflater<ReversedOptionDialogViewBinding>(ReversedOptionDialogViewBinding(layoutInflater), themeWrapper, payload)
 
 internal class VerticalReversedOptionDialogViewInflater(
     layoutInflater: LayoutInflater,
-    themeWrapper: AlertThemeWrapper,
+    themeWrapper: AlertDialogConfiguration,
     payload: DialogPayload.Option
 ) : DefaultReversedOptionDialogViewInflater<VerticalReversedOptionDialogViewBinding>(
     VerticalReversedOptionDialogViewBinding(layoutInflater),

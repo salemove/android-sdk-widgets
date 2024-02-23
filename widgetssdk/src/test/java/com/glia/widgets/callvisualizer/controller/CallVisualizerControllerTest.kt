023b27a40438743d9421038c315dd0a677ef6d33
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-02-23 12:04:54 +0200

    MOB 3111
    Fix broken tests and add new ones

---
+++ widgetssdk/src/test/java/com/glia/widgets/callvisualizer/controller/CallVisualizerControllerTest.kt
@@ -0,0 +1,205 @@
+package com.glia.widgets.callvisualizer.controller
+
+import com.glia.androidsdk.IncomingEngagementRequest
+import com.glia.widgets.core.dialog.DialogContract
+import com.glia.widgets.core.dialog.domain.ConfirmationDialogLinksUseCase
+import com.glia.widgets.core.dialog.model.DialogState
+import com.glia.widgets.core.dialog.model.Link
+import com.glia.widgets.core.engagement.domain.ConfirmationDialogUseCase
+import com.glia.widgets.engagement.State
+import com.glia.widgets.engagement.domain.EngagementRequestUseCase
+import com.glia.widgets.engagement.domain.EngagementStateUseCase
+import io.mockk.CapturingSlot
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.processors.PublishProcessor
+import io.reactivex.schedulers.Schedulers
+import junit.framework.TestCase.assertTrue
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class CallVisualizerControllerTest {
+    private val incomingEngagementProcessor: PublishProcessor<IncomingEngagementRequest> = PublishProcessor.create()
+    private val engagementStateProcessor: PublishProcessor<State> = PublishProcessor.create()
+
+    private lateinit var dialogController: DialogContract.Controller
+    private lateinit var confirmationDialogUseCase: ConfirmationDialogUseCase
+    private lateinit var engagementRequestUseCase: EngagementRequestUseCase
+    private lateinit var engagementStateUseCase: EngagementStateUseCase
+    private lateinit var confirmationDialogLinksUseCase: ConfirmationDialogLinksUseCase
+
+    private lateinit var dialogCallback: CapturingSlot<DialogContract.Controller.Callback>
+
+    private lateinit var controller: CallVisualizerContract.Controller
+
+    @Before
+    fun setUp() {
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { Schedulers.trampoline() }
+
+        dialogController = mockk(relaxUnitFun = true)
+        confirmationDialogUseCase = mockk(relaxUnitFun = true)
+
+        engagementRequestUseCase = mockk(relaxUnitFun = true)
+        every { engagementRequestUseCase() } returns incomingEngagementProcessor
+
+        engagementStateUseCase = mockk()
+        every { engagementStateUseCase() } returns engagementStateProcessor
+
+        confirmationDialogLinksUseCase = mockk(relaxed = true)
+
+        dialogCallback = slot()
+
+        controller = CallVisualizerController(
+            dialogController,
+            confirmationDialogUseCase,
+            engagementRequestUseCase,
+            engagementStateUseCase,
+            confirmationDialogLinksUseCase
+        )
+
+        verify { dialogController.addCallback(capture(dialogCallback)) }
+        verify { engagementRequestUseCase() }
+        verify { engagementStateUseCase() }
+    }
+
+    @After
+    fun tearDown() {
+        RxAndroidPlugins.reset()
+
+        confirmVerified(dialogController, confirmationDialogUseCase, engagementRequestUseCase, engagementStateUseCase, confirmationDialogLinksUseCase)
+    }
+
+    @Test
+    fun `CV engagement start will trigger will dismiss visitor code dialog`() {
+        engagementStateProcessor.onNext(State.StartedCallVisualizer)
+        verify { dialogController.dismissVisitorCodeDialog() }
+    }
+
+    @Test
+    fun `CV engagement end will emit engagementEndFlow`() {
+        val engagementEnd = controller.engagementEndFlow.test()
+
+        verify { engagementStateUseCase() }
+
+        engagementStateProcessor.onNext(State.FinishedCallVisualizer)
+
+        engagementEnd.assertNotComplete().assertValueCount(1)
+    }
+
+    @Test
+    fun `test dialog state mapper`() {
+        val testState = controller.state.map { it.value }.test()
+
+        emitDialogState(DialogState.VisitorCode)
+        emitDialogState(DialogState.None)
+        emitDialogState(DialogState.CVConfirmation)
+
+        testState.assertNotComplete().assertValueCount(3).values().run {
+            assertTrue(get(0) is CallVisualizerContract.State.DisplayVisitorCodeDialog)
+            assertTrue(get(1) is CallVisualizerContract.State.DismissDialog)
+            assertTrue(get(2) is CallVisualizerContract.State.DisplayConfirmationDialog)
+        }
+
+        verify { confirmationDialogLinksUseCase() }
+    }
+
+    @Test
+    fun `showVisitorCodeDialog shows visitor code dialog`() {
+        controller.showVisitorCodeDialog()
+        verify { dialogController.showVisitorCodeDialog() }
+    }
+
+    @Test
+    fun `engagement request should show visitor confirmation dialog when it is necessary`() {
+        val confirmationSlot = slot<(shouldShow: Boolean) -> Unit>()
+        incomingEngagementProcessor.onNext(mockk())
+        verify { dialogController.dismissVisitorCodeDialog() }
+        verify { confirmationDialogUseCase(capture(confirmationSlot)) }
+
+        confirmationSlot.captured.invoke(true)
+        verify { dialogController.showCVEngagementConfirmationDialog() }
+        verify(exactly = 0) { engagementRequestUseCase.accept(any()) }
+    }
+
+    @Test
+    fun `engagement request should accept engagement request when confirmation is not necessary`() {
+        val assetId = "asset_id"
+        controller.saveVisitorContextAssetId(assetId)
+
+        val confirmationSlot = slot<(shouldShow: Boolean) -> Unit>()
+        incomingEngagementProcessor.onNext(mockk())
+        verify { dialogController.dismissVisitorCodeDialog() }
+        verify { confirmationDialogUseCase(capture(confirmationSlot)) }
+
+        confirmationSlot.captured.invoke(false)
+        verify(exactly = 0) { dialogController.showCVEngagementConfirmationDialog() }
+        verify { engagementRequestUseCase.accept(eq(assetId)) }
+    }
+
+    @Test
+    fun `onLinkClicked will produce OpenWebBrowserScreen state`() {
+        val link = Link("title", "url")
+
+        val testState = controller.state.test()
+
+        controller.onLinkClicked(link)
+
+        verify { dialogController.dismissCurrentDialog() }
+
+        testState.assertNotComplete().assertValue {
+            it.value == CallVisualizerContract.State.OpenWebBrowserScreen(link.title, link.url)
+        }
+    }
+
+    @Test
+    fun `onEngagementConfirmationDialogAllowed will accept engagement request`() {
+        val testState = controller.state.test()
+
+        controller.onEngagementConfirmationDialogAllowed()
+
+        testState.assertNotComplete().assertValue { it.value is CallVisualizerContract.State.CloseHolderActivity }
+        verify { engagementRequestUseCase.accept(any()) }
+        verify { dialogController.dismissCurrentDialog() }
+
+    }
+
+    @Test
+    fun `onEngagementConfirmationDialogDeclined will decline engagement request`() {
+        val testState = controller.state.test()
+
+        controller.onEngagementConfirmationDialogDeclined()
+
+        testState.assertNotComplete().assertValue { it.value is CallVisualizerContract.State.CloseHolderActivity }
+        verify { engagementRequestUseCase.decline() }
+        verify { dialogController.dismissCurrentDialog() }
+
+    }
+
+    @Test
+    fun `dismissVisitorCodeDialog will dismiss visitor code dialog`() {
+        val testState = controller.state.test()
+
+        controller.dismissVisitorCodeDialog()
+
+        testState.assertNotComplete().assertValue { it.value is CallVisualizerContract.State.CloseHolderActivity }
+        verify { dialogController.dismissVisitorCodeDialog() }
+
+    }
+
+    @Test
+    fun `onWebBrowserOpened will request to show CV Confirmation Dialog`() {
+        controller.onWebBrowserOpened()
+
+        verify { dialogController.showCVEngagementConfirmationDialog() }
+    }
+
+
+    private fun emitDialogState(state: DialogState) {
+        dialogCallback.captured.emitDialogState(state)
+    }
+}

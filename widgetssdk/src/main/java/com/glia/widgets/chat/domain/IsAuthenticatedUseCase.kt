480868216510c67cac8cef420e7263aa49c84ae4
Andriy Shevtsov <andriy.shevtsov@masterofcode.com>
2024-03-01 01:23:48 +0200

    Do not hardcode the FORBIDDEN_DURING_ENGAGEMENT behavior in IsAuthenticatedUseCase
    MOB-3153

--- widgetssdk/src/main/java/com/glia/widgets/chat/domain/IsAuthenticatedUseCase.kt
+++ widgetssdk/src/main/java/com/glia/widgets/chat/domain/IsAuthenticatedUseCase.kt
@@ -2,6 +2,6 @@ package com.glia.widgets.chat.domain
 
 import com.glia.androidsdk.visitor.Authentication
 
-internal class IsAuthenticatedUseCase(private val authentication: Authentication) {
-    operator fun invoke(): Boolean = authentication.isAuthenticated
+internal class IsAuthenticatedUseCase(private val authentication: Authentication?) {
+    operator fun invoke(): Boolean = authentication?.isAuthenticated == true
 }

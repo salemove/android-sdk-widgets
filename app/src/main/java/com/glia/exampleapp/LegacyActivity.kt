7aae9ef014be491386b2b5bb20cdc5c4ca450400
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-03-05 23:00:59 +0200

    Add legacy Activity
    MOB 3143

---
+++ app/src/main/java/com/glia/exampleapp/LegacyActivity.kt
@@ -0,0 +1,15 @@
+package com.glia.exampleapp
+
+import android.os.Bundle
+import android.widget.ImageButton
+
+class LegacyActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_legacy)
+        findViewById<ImageButton>(R.id.close_button).setOnClickListener {
+            finish()
+        }
+    }
+}

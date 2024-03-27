e44b4c13807cc818c1562d7615a429717406d0ca
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-03-27 22:18:55 +0200

    Migrate all capture image and get content actions to activity result api
    MOB 3220

--- widgetssdk/src/main/java/com/glia/widgets/di/ControllerFactory.java
+++ widgetssdk/src/main/java/com/glia/widgets/di/ControllerFactory.java
@@ -130,7 +130,9 @@ public class ControllerFactory {
                 useCaseFactory.getIsQueueingOrEngagementUseCase(),
                 useCaseFactory.getQueueForEngagementUseCase(),
                 useCaseFactory.getDecideOnQueueingUseCase(),
-                useCaseFactory.getScreenSharingUseCase()
+                useCaseFactory.getScreenSharingUseCase(),
+                useCaseFactory.getTakePictureUseCase(),
+                useCaseFactory.getUriToFileAttachmentUseCase()
             );
         }
 
@@ -293,7 +295,9 @@ public class ControllerFactory {
             useCaseFactory.createEnableSendMessageButtonUseCase(),
             useCaseFactory.createShowMessageLimitErrorUseCase(),
             useCaseFactory.createResetMessageCenterUseCase(),
-            createDialogController()
+            createDialogController(),
+            useCaseFactory.getTakePictureUseCase(),
+            useCaseFactory.getUriToFileAttachmentUseCase()
         );
     }
 

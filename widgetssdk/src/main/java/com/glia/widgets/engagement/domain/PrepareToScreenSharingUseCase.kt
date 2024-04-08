0a46a9bee6544e4398577ce54d1742afd8db42f4
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-04-08 19:41:25 +0300

    Fix screen-sharing for API 34
    - Upgrade core and appCompat to the maximum versions supporting api-33
    - Refactoring
    - Add comments describing the Media projection requirements
    
    MOB 3232

---
+++ widgetssdk/src/main/java/com/glia/widgets/engagement/domain/PrepareToScreenSharingUseCase.kt
@@ -0,0 +1,23 @@
+package com.glia.widgets.engagement.domain
+
+import android.os.Build
+import com.glia.widgets.core.notification.domain.ShowScreenSharingNotificationUseCase
+
+internal interface PrepareToScreenSharingUseCase {
+    operator fun invoke()
+}
+
+internal class PrepareToScreenSharingUseCaseImpl(
+    private val showScreenSharingNotificationUseCase: ShowScreenSharingNotificationUseCase,
+    private val startMediaProjectionServiceUseCase: StartMediaProjectionServiceUseCase,
+    private val informThatReadyToShareScreenUseCase: InformThatReadyToShareScreenUseCase
+) : PrepareToScreenSharingUseCase {
+    override fun invoke() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startMediaProjectionServiceUseCase()
+        } else {
+            informThatReadyToShareScreenUseCase()
+            showScreenSharingNotificationUseCase()
+        }
+    }
+}

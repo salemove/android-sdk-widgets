f3630e16f1c727174c5db21739158548574bf0cc
Andrii Horishnii <andrii.horishnii@masterofcode.com>
2024-09-12 23:04:37 +0300

    Implement Entry Widget (embedded view)
    MOB-3478

---
+++ widgetssdk/src/main/java/com/glia/widgets/entrywidget/adapter/EntryWidgetPoweredByViewHolder.kt
@@ -0,0 +1,14 @@
+package com.glia.widgets.entrywidget.adapter
+
+import com.glia.widgets.R
+import com.glia.widgets.databinding.EntryWidgetPoweredByItemBinding
+import com.glia.widgets.helper.setLocaleText
+
+internal class EntryWidgetPoweredByViewHolder(
+    private val binding: EntryWidgetPoweredByItemBinding
+) : EntryWidgetAdapter.ViewHolder(binding.root) {
+
+    init {
+        binding.title.setLocaleText(R.string.general_powered)
+    }
+}

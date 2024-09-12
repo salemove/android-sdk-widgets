4d7a0002eb68d2b821c98dbfcfb54d4fdf3636c4
Andrii Horishnii <andrii.horishnii@masterofcode.com>
2024-09-12 21:12:07 +0300

    Implement Entry Widget (bottom sheet)
    MOB-3476

---
+++ widgetssdk/src/main/java/com/glia/widgets/entrywidget/EntryWidgetActivity.kt
@@ -0,0 +1,24 @@
+package com.glia.widgets.entrywidget
+
+import android.os.Bundle
+import com.glia.widgets.base.FadeTransitionActivity
+
+/**
+ * EntryWidgetActivity provides a way to display the EntryWidget bottom sheet.
+ */
+class EntryWidgetActivity : FadeTransitionActivity(), EntryWidgetFragment.OnDismissListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (savedInstanceState == null) {
+            EntryWidgetFragment().show(supportFragmentManager)
+        }
+    }
+
+    override fun onEntryWidgetDismiss() {
+        if (!isChangingConfigurations) {
+            finish()
+        }
+    }
+}

1597c088a12135ac4ddae70a6c667b4700425ec4
Andrii Horishnii <andrii.horishnii@masterofcode.com>
2024-09-12 23:04:37 +0300

    Implement Entry Widget (embedded view)
    MOB-3478

---
+++ widgetssdk/src/main/java/com/glia/widgets/entrywidget/EntryWidgetView.kt
@@ -0,0 +1,44 @@
+package com.glia.widgets.entrywidget
+
+import android.content.Context
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.glia.widgets.R
+import com.glia.widgets.di.Dependencies
+import com.glia.widgets.entrywidget.adapter.EntryWidgetAdapter
+import com.glia.widgets.entrywidget.adapter.EntryWidgetItemDecoration
+import com.glia.widgets.helper.getDrawableCompat
+
+internal class EntryWidgetView(
+    context: Context
+) : RecyclerView(context, null, 0), EntryWidgetContract.View {
+    var onDismissListener: (() -> Unit)? = null
+
+    private lateinit var controller: EntryWidgetContract.Controller
+    private val adapter = EntryWidgetAdapter().also { setAdapter(it) }
+
+    init {
+        layoutManager = LinearLayoutManager(context)
+        getDrawableCompat(R.drawable.bg_separator_gray)?.let {
+            addItemDecoration(EntryWidgetItemDecoration(it))
+        }
+        adapter.onItemClickListener = {
+            controller.onItemClicked(it)
+        }
+
+        setController(Dependencies.controllerFactory.entryWidgetController)
+    }
+
+    override fun setController(controller: EntryWidgetContract.Controller) {
+        this.controller = controller
+        controller.setView(this)
+    }
+
+    override fun showItems(items: List<EntryWidgetContract.ItemType>) {
+        adapter.items = items
+    }
+
+    override fun dismiss() {
+        onDismissListener?.invoke()
+    }
+}

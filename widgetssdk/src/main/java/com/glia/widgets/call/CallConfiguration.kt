d92cb8cf7969a1375523e83ae0bdd3255d775cc0
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-09-16 22:42:16 +0300

    Refactor configurations
    - Migrate Call and Chat Activities to Kotlin
    - Refactor CallConfiguration
    - Refactor EngagementConfiguration
    
    MOB 3587

--- widgetssdk/src/main/java/com/glia/widgets/call/CallConfiguration.kt
+++ widgetssdk/src/main/java/com/glia/widgets/call/CallConfiguration.kt
@@ -2,49 +2,12 @@ package com.glia.widgets.call
 
 import com.glia.androidsdk.Engagement
 import com.glia.widgets.core.configuration.EngagementConfiguration
-import com.glia.widgets.helper.Utils
 
-internal class CallConfiguration private constructor(builder: Builder) {
+internal data class CallConfiguration @JvmOverloads constructor(
     @JvmField
-    val engagementConfiguration: EngagementConfiguration?
+    val engagementConfiguration: EngagementConfiguration?,
     @JvmField
-    val mediaType: Engagement.MediaType?
+    val mediaType: Engagement.MediaType? = null,
     @JvmField
-    val isUpgradeToCall: Boolean
-
-    init {
-        engagementConfiguration = builder.engagementConfiguration
-        mediaType = builder.mediaType
-        isUpgradeToCall = builder.isUpgradeToCall
-    }
-
-    class Builder {
-        var engagementConfiguration: EngagementConfiguration? = null
-            private set
-        var mediaType: Engagement.MediaType? = null
-            private set
-        var isUpgradeToCall = false
-            private set
-
-        fun setEngagementConfiguration(engagementConfiguration: EngagementConfiguration?) = apply {
-            this.engagementConfiguration = engagementConfiguration
-        }
-
-        fun setMediaType(mediaType: Engagement.MediaType?) = apply {
-            this.mediaType = mediaType
-        }
-
-        fun setMediaType(mediaType: String) = apply {
-            this.mediaType = Utils.toMediaType(mediaType)
-        }
-
-        fun setIsUpgradeToCall(isUpgradeToCall: Boolean) = apply {
-            this.isUpgradeToCall = isUpgradeToCall
-        }
-
-
-        fun build(): CallConfiguration {
-            return CallConfiguration(this)
-        }
-    }
-}
+    val isUpgradeToCall: Boolean = false
+)

ec3794ce232110674fe4a0ea5bf7d7f4e78dcb7b
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-09-18 09:52:51 +0300

    Separate Intent-related functionality
    - Moved all the Intent-related functionality to the IntentHelper class.
    - Abstraction will let us to easily mock this layer.
    
    MOB 3582

--- widgetssdk/src/main/java/com/glia/widgets/core/notification/Extensions.kt
+++ widgetssdk/src/main/java/com/glia/widgets/core/notification/Extensions.kt
@@ -2,24 +2,8 @@ package com.glia.widgets.core.notification
 
 import android.app.NotificationManager
 import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import android.os.Build
-import android.provider.Settings
 import androidx.core.app.NotificationManagerCompat
-
-internal fun Context.openNotificationChannelScreen() {
-    val intent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
-            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            .putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
-    } else {
-        Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            .setData(Uri.fromParts("package", packageName, null))
-    }
-    startActivity(intent)
-}
 
 internal fun Context.areNotificationsEnabled(): Boolean =
     NotificationManagerCompat.from(this).areNotificationsEnabled()

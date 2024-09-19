03e0182fb3ba43c7f8215f8b532a0b9d8669ce1f
Andrii Horishnii <andrii.horishnii@masterofcode.com>
2024-09-19 16:52:28 +0300

    Unified UI customization for Entry Widget and SCv2
    MOB-3591

--- widgetssdk/src/main/java/com/glia/widgets/view/unifiedui/theme/UnifiedTheme.kt
+++ widgetssdk/src/main/java/com/glia/widgets/view/unifiedui/theme/UnifiedTheme.kt
@@ -7,10 +7,11 @@ import com.glia.widgets.view.unifiedui.theme.bubble.BubbleTheme
 import com.glia.widgets.view.unifiedui.theme.call.CallTheme
 import com.glia.widgets.view.unifiedui.theme.callvisulaizer.CallVisualizerTheme
 import com.glia.widgets.view.unifiedui.theme.chat.ChatTheme
+import com.glia.widgets.view.unifiedui.theme.entrywidget.EntryWidgetTheme
 import com.glia.widgets.view.unifiedui.theme.secureconversations.SecureConversationsConfirmationScreenTheme
 import com.glia.widgets.view.unifiedui.theme.secureconversations.SecureConversationsWelcomeScreenTheme
 import com.glia.widgets.view.unifiedui.theme.survey.SurveyTheme
-import com.glia.widgets.view.unifiedui.webbrowser.WebBrowserTheme
+import com.glia.widgets.view.unifiedui.theme.webbrowser.WebBrowserTheme
 
 internal data class UnifiedTheme(
     val alertTheme: AlertTheme? = null,
@@ -22,7 +23,8 @@ internal data class UnifiedTheme(
     val secureConversationsWelcomeScreenTheme: SecureConversationsWelcomeScreenTheme? = null,
     val secureConversationsConfirmationScreenTheme: SecureConversationsConfirmationScreenTheme? = null,
     val snackBarTheme: SnackBarTheme? = null,
-    val webBrowserTheme: WebBrowserTheme? = null
+    val webBrowserTheme: WebBrowserTheme? = null,
+    val entryWidgetTheme: EntryWidgetTheme? = null
 ) : Mergeable<UnifiedTheme> {
     override fun merge(other: UnifiedTheme): UnifiedTheme = UnifiedTheme(
         alertTheme = alertTheme merge other.alertTheme,
@@ -34,7 +36,8 @@ internal data class UnifiedTheme(
         secureConversationsWelcomeScreenTheme = secureConversationsWelcomeScreenTheme merge other.secureConversationsWelcomeScreenTheme,
         secureConversationsConfirmationScreenTheme = secureConversationsConfirmationScreenTheme merge other.secureConversationsConfirmationScreenTheme,
         snackBarTheme = snackBarTheme merge other.snackBarTheme,
-        webBrowserTheme = webBrowserTheme merge other.webBrowserTheme
+        webBrowserTheme = webBrowserTheme merge other.webBrowserTheme,
+        entryWidgetTheme = entryWidgetTheme merge other.entryWidgetTheme
     )
 
 }

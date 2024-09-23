a25eb6fa743ed97f0013c917369b385ed9f74540
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-09-23 12:45:18 +0300

    Devs want to create public interfaces for EngagementLauncher
    - Create interfaces for EngagementLauncher
    - Create basic ConfigurationManager that should hold all the init config
    
    MOB 3588

--- widgetssdk/src/main/java/com/glia/widgets/view/unifiedui/theme/UnifiedThemeManager.kt
+++ widgetssdk/src/main/java/com/glia/widgets/view/unifiedui/theme/UnifiedThemeManager.kt
@@ -5,7 +5,7 @@ import com.glia.widgets.view.unifiedui.parse.RemoteConfigurationParser
 internal class UnifiedThemeManager {
     private val parser: RemoteConfigurationParser by lazy { RemoteConfigurationParser() }
 
-    internal var theme: UnifiedTheme? = null
+    var theme: UnifiedTheme? = null
 
     fun applyJsonConfig(jsonConfig: String?) {
         theme = jsonConfig?.let(parser::parseRemoteConfiguration)?.toUnifiedTheme()

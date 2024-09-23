5c8820cf673e0a089719cd77d717af69a0983621
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-09-23 12:45:18 +0300

    Devs want to create public interfaces for EngagementLauncher
    - Create interfaces for EngagementLauncher
    - Create basic ConfigurationManager that should hold all the init config
    
    MOB 3588

---
+++ widgetssdk/src/main/java/com/glia/widgets/launcher/EngagementLauncher.kt
@@ -0,0 +1,57 @@
+package com.glia.widgets.launcher
+
+import android.app.Activity
+import com.glia.androidsdk.Engagement
+
+/**
+ * An interface for launching different types of engagements, such as chat,
+ * audio calls, video calls, and secure messaging
+ */
+interface EngagementLauncher {
+    /**
+     * Starts a chat engagement.
+     *
+     * @param activity The Activity used to launch the chat screen
+     */
+    fun startChat(activity: Activity)
+
+    /**
+     * Starts an audio engagement.
+     *
+     * @param activity The Activity used to launch the call screen
+     */
+    fun startAudioCall(activity: Activity)
+
+    /**
+     * Starts a video engagement.
+     *
+     * @param activity The Activity used to launch the call screen
+     */
+    fun startVideoCall(activity: Activity)
+
+    /**
+     * Starts a secure messaging.
+     *
+     * @param activity The Activity used to launch the secure messaging welcome or chat screen
+     */
+    fun startSecureMessaging(activity: Activity)
+}
+
+internal class EngagementLauncherImpl(private val activityLauncher: ActivityLauncher) : EngagementLauncher {
+
+    override fun startChat(activity: Activity) {
+        activityLauncher.launchChat(activity)
+    }
+
+    override fun startAudioCall(activity: Activity) {
+        activityLauncher.launchCall(activity, Engagement.MediaType.AUDIO)
+    }
+
+    override fun startVideoCall(activity: Activity) {
+        activityLauncher.launchCall(activity, Engagement.MediaType.VIDEO)
+    }
+
+    override fun startSecureMessaging(activity: Activity) {
+        activityLauncher.launchSecureMessagingWelcomeScreen(activity)
+    }
+}

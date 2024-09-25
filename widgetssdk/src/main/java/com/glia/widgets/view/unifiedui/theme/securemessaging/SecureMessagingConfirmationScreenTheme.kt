5d38a7088098ec326f278dcf8df22e79607264cd
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-09-25 14:41:48 +0300

    Rename all the Unified UI related classes to new SecureMessaging... way

--- widgetssdk/src/main/java/com/glia/widgets/view/unifiedui/theme/securemessaging/SecureMessagingConfirmationScreenTheme.kt
+++ widgetssdk/src/main/java/com/glia/widgets/view/unifiedui/theme/securemessaging/SecureMessagingConfirmationScreenTheme.kt
@@ -1,4 +1,4 @@
-package com.glia.widgets.view.unifiedui.theme.secureconversations
+package com.glia.widgets.view.unifiedui.theme.securemessaging
 
 import com.glia.widgets.view.unifiedui.Mergeable
 import com.glia.widgets.view.unifiedui.merge
@@ -7,16 +7,16 @@ import com.glia.widgets.view.unifiedui.theme.base.ColorTheme
 import com.glia.widgets.view.unifiedui.theme.base.HeaderTheme
 import com.glia.widgets.view.unifiedui.theme.base.TextTheme
 
-internal data class SecureConversationsConfirmationScreenTheme(
+internal data class SecureMessagingConfirmationScreenTheme(
     val headerTheme: HeaderTheme? = null,
     val backgroundTheme: ColorTheme? = null,
     val iconColorTheme: ColorTheme? = null,
     val titleTheme: TextTheme? = null,
     val subtitleTheme: TextTheme? = null,
     val checkMessagesButtonTheme: ButtonTheme? = null
-) : Mergeable<SecureConversationsConfirmationScreenTheme> {
-    override fun merge(other: SecureConversationsConfirmationScreenTheme): SecureConversationsConfirmationScreenTheme =
-        SecureConversationsConfirmationScreenTheme(
+) : Mergeable<SecureMessagingConfirmationScreenTheme> {
+    override fun merge(other: SecureMessagingConfirmationScreenTheme): SecureMessagingConfirmationScreenTheme =
+        SecureMessagingConfirmationScreenTheme(
             headerTheme = headerTheme merge other.headerTheme,
             backgroundTheme = backgroundTheme merge other.backgroundTheme,
             iconColorTheme = iconColorTheme merge other.iconColorTheme,

e393e268b2151eafd6c5336a85e8224354829366
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-10-08 10:54:07 +0300

    Devs want to use cached Queues to check Secure Messaging availability
    MOB 3646

--- widgetssdk/src/main/java/com/glia/widgets/core/secureconversations/domain/IsMessagingAvailableUseCase.kt
+++ widgetssdk/src/main/java/com/glia/widgets/core/secureconversations/domain/IsMessagingAvailableUseCase.kt
@@ -2,12 +2,25 @@ package com.glia.widgets.core.secureconversations.domain
 
 import com.glia.androidsdk.queuing.Queue
 import com.glia.androidsdk.queuing.QueueState
+import com.glia.widgets.core.queue.QueueRepository
+import com.glia.widgets.core.queue.QueuesState
 import com.glia.widgets.helper.supportMessaging
+import io.reactivex.rxjava3.core.Flowable
 
-internal class IsMessagingAvailableUseCase {
+internal class IsMessagingAvailableUseCase(private val queueRepository: QueueRepository) {
 
-    operator fun invoke(queues: List<Queue>): Boolean = queues
-        .asSequence()
+    operator fun invoke(): Flowable<Result<Boolean>> = queueRepository.observableIntegratorQueues
+        .filter { it !is QueuesState.Loading }
+        .map { mapResult(it) }
+        .distinctUntilChanged()
+
+    private fun mapResult(queueMonitorState: QueuesState): Result<Boolean> = when (queueMonitorState) {
+        is QueuesState.Error -> Result.failure(queueMonitorState.error)
+        is QueuesState.Queues -> Result.success(isMessagingAvailable(queueMonitorState.queues))
+        else -> Result.success(false)
+    }
+
+    private fun isMessagingAvailable(queues: List<Queue>): Boolean = queues.asSequence()
         .filterNot { it.state.status == QueueState.Status.CLOSED }
         .filterNot { it.state.status == QueueState.Status.UNKNOWN }
         .any { it.supportMessaging() }

451119a379a702f612989843a562447e1ecdadc5
Andriy Shevtsov <andriy.shevtsov@masterofcode.com>
2024-10-09 12:23:39 +0300

    Hide 'Secure Messaging' engagement type from Entry Widget if visitor is not authenticated
    MOB-3659

--- widgetssdk/src/main/java/com/glia/widgets/entrywidget/EntryWidgetController.kt
+++ widgetssdk/src/main/java/com/glia/widgets/entrywidget/EntryWidgetController.kt
@@ -4,14 +4,17 @@ import android.annotation.SuppressLint
 import com.glia.androidsdk.Engagement
 import com.glia.androidsdk.queuing.Queue
 import com.glia.androidsdk.queuing.QueueState
+import com.glia.widgets.chat.domain.IsAuthenticatedUseCase
 import com.glia.widgets.core.queue.QueueRepository
 import com.glia.widgets.core.queue.QueuesState
 import com.glia.widgets.helper.Logger
 import com.glia.widgets.helper.TAG
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 
-internal class EntryWidgetController(private val queueRepository: QueueRepository) :
-    EntryWidgetContract.Controller {
+internal class EntryWidgetController(
+    private val queueRepository: QueueRepository,
+    private val isAuthenticatedUseCase: IsAuthenticatedUseCase
+) : EntryWidgetContract.Controller {
     private lateinit var view: EntryWidgetContract.View
 
     @SuppressLint("CheckResult")
@@ -48,7 +51,7 @@ internal class EntryWidgetController(private val queueRepository: QueueRepositor
                     if (allMedias.contains(Engagement.MediaType.TEXT)) add(EntryWidgetContract.ItemType.CHAT)
                     if (allMedias.contains(Engagement.MediaType.AUDIO)) add(EntryWidgetContract.ItemType.AUDIO_CALL)
                     if (allMedias.contains(Engagement.MediaType.VIDEO)) add(EntryWidgetContract.ItemType.VIDEO_CALL)
-                    if (allMedias.contains(Engagement.MediaType.MESSAGING)) add(EntryWidgetContract.ItemType.SECURE_MESSAGE)
+                    if (allMedias.contains(Engagement.MediaType.MESSAGING) && isAuthenticatedUseCase()) add(EntryWidgetContract.ItemType.SECURE_MESSAGE)
 
                     if (allMedias.isEmpty()) {
                         add(EntryWidgetContract.ItemType.EMPTY_STATE)

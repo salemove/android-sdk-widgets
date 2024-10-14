c2dd105c008e6f8cc223aef41b54557c92c425e6
Andrii Horishnii <andrii.horishnii@masterofcode.com>
2024-10-14 16:08:30 +0300

    Update snapshots for the Entry Widget Bottom Sheet
    MOB-3544

---
+++ widgetssdk/src/testSnapshot/java/com/glia/widgets/entrywidget/EntryWidgetBottomSheetTest.kt
@@ -0,0 +1,28 @@
+package com.glia.widgets.entrywidget
+
+import android.view.View
+import androidx.core.view.allViews
+import com.glia.widgets.databinding.EntryWidgetFragmentBinding
+import com.glia.widgets.view.unifiedui.theme.UnifiedTheme
+
+internal class EntryWidgetBottomSheetTest : EntryWidgetEmbeddedViewTest() {
+    override fun setupView(
+        items: List<EntryWidgetContract.ItemType>,
+        viewType: EntryWidgetContract.ViewType,
+        unifiedTheme: UnifiedTheme?
+    ): View {
+        localeProviderMock()
+
+        val entryWidgetFragment = EntryWidgetFragment()
+        val binding = EntryWidgetFragmentBinding.inflate(layoutInflater)
+
+        entryWidgetFragment.setupView(context, binding, unifiedTheme?.entryWidgetTheme)
+
+        binding.container.allViews.forEach {
+            val entryWidgetView = it as? EntryWidgetView
+            entryWidgetView?.showItems(items)
+        }
+
+        return binding.root
+    }
+}

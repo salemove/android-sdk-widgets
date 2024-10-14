24a65790443c2c4db1b83cb7f70e4e266e122b40
Andrii Horishnii <andrii.horishnii@masterofcode.com>
2024-10-14 16:08:30 +0300

    Update snapshots for the Entry Widget Bottom Sheet
    MOB-3544

--- widgetssdk/src/testSnapshot/java/com/glia/widgets/entrywidget/EntryWidgetViewBottomSheetTypeTest.kt
+++
@@ -1,5 +0,0 @@
-package com.glia.widgets.entrywidget
-
-internal class EntryWidgetViewBottomSheetTypeTest : EntryWidgetViewEmbeddedViewTypeTest() {
-    override val viewType: EntryWidgetContract.ViewType = EntryWidgetContract.ViewType.BOTTOM_SHEET
-}

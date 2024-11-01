d65c690cdae435bc23b64d4d033d0f1d93006f20
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-11-01 22:12:09 +0200

    Introduce Intention Enum for opening Chat screen

--- widgetssdk/src/main/java/com/glia/widgets/core/secureconversations/domain/IsSecureEngagementUseCase.kt
+++ widgetssdk/src/main/java/com/glia/widgets/core/secureconversations/domain/IsSecureEngagementUseCase.kt
@@ -1,14 +1,13 @@
 package com.glia.widgets.core.secureconversations.domain
 
-import com.glia.widgets.chat.ChatType
-import com.glia.widgets.core.engagement.GliaEngagementConfigRepository
 import com.glia.widgets.engagement.domain.IsQueueingOrEngagementUseCase
 
+/*TODO overall think about do we need such a use case? Or it is better to pass the current engagement type to the places where we need to differentiate btw LIVE and SC engagements.
+   We need all this repositories and use cases from the chat screen, so it might be clear*/
 internal class IsSecureEngagementUseCase(
-    private val engagementConfigRepository: GliaEngagementConfigRepository,
     private val isQueueingOrEngagementUseCase: IsQueueingOrEngagementUseCase
 ) {
     operator fun invoke(): Boolean {
-        return engagementConfigRepository.chatType == ChatType.SECURE_MESSAGING && !isQueueingOrEngagementUseCase()
+        return /* TODO check here for current engagement being SC */ !isQueueingOrEngagementUseCase()
     }
 }

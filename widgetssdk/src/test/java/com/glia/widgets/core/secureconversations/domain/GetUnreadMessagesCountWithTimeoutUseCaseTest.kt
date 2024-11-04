bf879e33846aac6050d6b12ac9d46ea481ff3888
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-11-04 23:05:55 +0200

    Make SC APIs work seamlessly when sdk is not initialized

--- widgetssdk/src/test/java/com/glia/widgets/core/secureconversations/domain/GetUnreadMessagesCountWithTimeoutUseCaseTest.kt
+++ widgetssdk/src/test/java/com/glia/widgets/core/secureconversations/domain/GetUnreadMessagesCountWithTimeoutUseCaseTest.kt
@@ -2,7 +2,9 @@ package com.glia.widgets.core.secureconversations.domain
 
 import com.glia.androidsdk.GliaException
 import com.glia.androidsdk.RequestCallback
+import com.glia.widgets.chat.domain.IsAuthenticatedUseCase
 import com.glia.widgets.core.secureconversations.SecureConversationsRepository
+import com.glia.widgets.di.GliaCore
 import io.reactivex.rxjava3.plugins.RxJavaPlugins
 import org.junit.Before
 import org.junit.Test
@@ -20,16 +22,37 @@ import kotlin.properties.Delegates
 class GetUnreadMessagesCountWithTimeoutUseCaseTest {
     private var repository: SecureConversationsRepository by Delegates.notNull()
     private var useCase: GetUnreadMessagesCountWithTimeoutUseCase by Delegates.notNull()
+    private var isAuthenticatedUseCase: IsAuthenticatedUseCase by Delegates.notNull()
+    private var core: GliaCore by Delegates.notNull()
 
     @Before
     fun setUp() {
         RxJavaPlugins.reset()
         repository = mock()
-        useCase = GetUnreadMessagesCountWithTimeoutUseCase(repository)
+        isAuthenticatedUseCase = mock()
+        core = mock()
+        useCase = GetUnreadMessagesCountWithTimeoutUseCase(repository, isAuthenticatedUseCase, core)
+    }
+
+    @Test
+    fun `invoke returns NO_UNREAD_MESSAGES when GliaCore is not initialized`() {
+        whenever(core.isInitialized).thenReturn(false)
+
+        useCase().test().assertComplete().assertValue(NO_UNREAD_MESSAGES)
+    }
+
+    @Test
+    fun `invoke returns NO_UNREAD_MESSAGES when user is not authenticated`() {
+        whenever(core.isInitialized).thenReturn(true)
+        whenever(isAuthenticatedUseCase()).thenReturn(false)
+
+        useCase().test().assertComplete().assertValue(NO_UNREAD_MESSAGES)
     }
 
     @Test
     fun `invoke returns NO_UNREAD_MESSAGES when there is no answer during timeout`() {
+        mockInitializedAndAuthenticated()
+
         val answer: Answer<*> = AnswersWithDelay(
             TIMEOUT_SEC * 2_000L
         ) {
@@ -43,6 +66,8 @@ class GetUnreadMessagesCountWithTimeoutUseCaseTest {
 
     @Test
     fun `invoke completes with NO_UNREAD_MESSAGES when error is returned`() {
+        mockInitializedAndAuthenticated()
+
         doAnswer {
             val callback: RequestCallback<Int?> = it.getArgument(0)
             callback.onResult(null, GliaException("", GliaException.Cause.INTERNAL_ERROR))
@@ -53,6 +78,8 @@ class GetUnreadMessagesCountWithTimeoutUseCaseTest {
 
     @Test
     fun `invoke completes with NO_UNREAD_MESSAGES when count is null`() {
+        mockInitializedAndAuthenticated()
+
         doAnswer {
             val callback: RequestCallback<Int?> = it.getArgument(0)
             callback.onResult(null, null)
@@ -63,6 +90,8 @@ class GetUnreadMessagesCountWithTimeoutUseCaseTest {
 
     @Test
     fun `invoke completes with NO_UNREAD_MESSAGES when count is 0`() {
+        mockInitializedAndAuthenticated()
+
         doAnswer {
             val callback: RequestCallback<Int?> = it.getArgument(0)
             callback.onResult(0, null)
@@ -73,11 +102,18 @@ class GetUnreadMessagesCountWithTimeoutUseCaseTest {
 
     @Test
     fun `invoke completes with correct messages count when success is called`() {
+        mockInitializedAndAuthenticated()
+
         doAnswer {
             val callback: RequestCallback<Int?> = it.getArgument(0)
             callback.onResult(10, null)
         }.whenever(repository).getUnreadMessagesCount(any())
 
         useCase().test().assertComplete().assertValue(10)
+    }
+
+    private fun mockInitializedAndAuthenticated() {
+        whenever(core.isInitialized).thenReturn(true)
+        whenever(isAuthenticatedUseCase()).thenReturn(true)
     }
 }

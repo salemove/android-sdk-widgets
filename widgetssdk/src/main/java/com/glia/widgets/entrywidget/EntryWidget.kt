aaffd5e9035f2e4c5d6a5dffc9060774a857e7cc
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-11-04 23:05:55 +0200

    Make SC APIs work seamlessly when sdk is not initialized

--- widgetssdk/src/main/java/com/glia/widgets/entrywidget/EntryWidget.kt
+++ widgetssdk/src/main/java/com/glia/widgets/entrywidget/EntryWidget.kt
@@ -3,7 +3,7 @@ package com.glia.widgets.entrywidget
 import android.app.Activity
 import android.content.Context
 import android.view.View
-import com.glia.widgets.core.secureconversations.SecureConversationsRepository
+import com.glia.widgets.core.secureconversations.domain.HasPendingSecureConversationsWithTimeoutUseCase
 import com.glia.widgets.entrywidget.adapter.EntryWidgetAdapter
 import com.glia.widgets.helper.unSafeSubscribe
 import com.glia.widgets.launcher.ActivityLauncher
@@ -37,11 +37,11 @@ internal class EntryWidgetImpl(
     private val activityLauncher: ActivityLauncher,
     private val themeManager: UnifiedThemeManager,
     private val entryWidgetHideController: EntryWidgetHideController,
-    private val secureConversationsRepository: SecureConversationsRepository
+    private val hasPendingSecureConversationsWithTimeoutUseCase: HasPendingSecureConversationsWithTimeoutUseCase
 ) : EntryWidget {
 
     override fun show(activity: Activity) {
-        secureConversationsRepository.getHasPendingSecureConversationsWithTimeout().unSafeSubscribe {
+        hasPendingSecureConversationsWithTimeoutUseCase().unSafeSubscribe {
             handleShowWithPendingSecureConversations(it, activity)
         }
     }

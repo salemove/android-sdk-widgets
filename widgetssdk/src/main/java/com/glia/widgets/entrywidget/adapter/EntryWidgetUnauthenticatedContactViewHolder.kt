bef3d3b2940198f24bc93bf6c9eb302c7b047afc
Andriy Shevtsov <andriy.shevtsov@masterofcode.com>
2024-11-06 22:31:33 +0200

    Show unread messages counter for 'Messaging' on embedded view Implement a separate View Holder for Secure Messaging item
    MOB-3746

--- widgetssdk/src/main/java/com/glia/widgets/entrywidget/adapter/EntryWidgetUnauthenticatedContactViewHolder.kt
+++ widgetssdk/src/main/java/com/glia/widgets/entrywidget/adapter/EntryWidgetUnauthenticatedContactViewHolder.kt
@@ -3,9 +3,8 @@ package com.glia.widgets.entrywidget.adapter
 import android.view.View
 import androidx.core.view.isVisible
 import com.glia.widgets.R
-import com.glia.widgets.databinding.EntryWidgetMediaTypeItemBinding
+import com.glia.widgets.databinding.EntryWidgetUnauthenticatedContactBinding
 import com.glia.widgets.entrywidget.EntryWidgetContract
-import com.glia.widgets.helper.applyShadow
 import com.glia.widgets.helper.setLocaleContentDescription
 import com.glia.widgets.helper.setLocaleHint
 import com.glia.widgets.helper.setLocaleText
@@ -14,8 +13,8 @@ import com.glia.widgets.view.unifiedui.applyLayerTheme
 import com.glia.widgets.view.unifiedui.applyTextTheme
 import com.glia.widgets.view.unifiedui.theme.entrywidget.MediaTypeItemTheme
 
-internal class EntryWidgetMediaTypeItemViewHolder(
-    private val binding: EntryWidgetMediaTypeItemBinding,
+internal class EntryWidgetUnauthenticatedContactViewHolder(
+    private val binding: EntryWidgetUnauthenticatedContactBinding,
     itemTheme: MediaTypeItemTheme?
 ) : EntryWidgetAdapter.ViewHolder(binding.root) {
 
@@ -43,24 +42,21 @@ internal class EntryWidgetMediaTypeItemViewHolder(
                 binding.description.setLocaleText(R.string.entry_widget_video_button_description)
                 binding.description.setLocaleHint(R.string.entry_widget_video_button_accessibility_hint)
             }
+
             EntryWidgetContract.ItemType.AUDIO_CALL -> {
                 binding.icon.setImageResource(R.drawable.ic_audio)
                 binding.title.setLocaleText(R.string.entry_widget_audio_button_label)
                 binding.description.setLocaleText(R.string.entry_widget_audio_button_description)
                 binding.description.setLocaleHint(R.string.entry_widget_audio_button_accessibility_hint)
             }
+
             EntryWidgetContract.ItemType.CHAT -> {
                 binding.icon.setImageResource(R.drawable.ic_chat)
                 binding.title.setLocaleText(R.string.entry_widget_live_chat_button_label)
                 binding.description.setLocaleText(R.string.entry_widget_live_chat_button_description)
                 binding.description.setLocaleHint(R.string.entry_widget_live_chat_button_accessibility_hint)
             }
-            EntryWidgetContract.ItemType.SECURE_MESSAGE -> {
-                binding.icon.setImageResource(R.drawable.ic_secure_message)
-                binding.title.setLocaleText(R.string.entry_widget_secure_messaging_button_label)
-                binding.description.setLocaleText(R.string.entry_widget_secure_messaging_button_description)
-                binding.description.setLocaleHint(R.string.entry_widget_secure_messaging_button_accessibility_hint)
-            }
+
             else -> {
                 binding.icon.setImageResource(0)
                 binding.title.text = null

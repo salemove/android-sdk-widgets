11ad1648ad782feaa5c8f606531d7cb7f755a6a9
Andriy Shevtsov <andriy.shevtsov@masterofcode.com>
2024-11-06 22:31:33 +0200

    Show unread messages counter for 'Messaging' on embedded view Implement a separate View Holder for Secure Messaging item
    MOB-3746

---
+++ widgetssdk/src/main/java/com/glia/widgets/entrywidget/adapter/EntryWidgetAuthenticatedContactViewHolder.kt
@@ -0,0 +1,45 @@
+package com.glia.widgets.entrywidget.adapter
+
+import android.view.View
+import androidx.core.view.isVisible
+import com.glia.widgets.R
+import com.glia.widgets.databinding.EntryWidgetAuthenticatedContactBinding
+import com.glia.widgets.entrywidget.EntryWidgetContract
+import com.glia.widgets.helper.setLocaleHint
+import com.glia.widgets.helper.setLocaleText
+import com.glia.widgets.view.unifiedui.applyImageColorTheme
+import com.glia.widgets.view.unifiedui.applyLayerTheme
+import com.glia.widgets.view.unifiedui.applyTextTheme
+import com.glia.widgets.view.unifiedui.theme.entrywidget.MediaTypeItemTheme
+
+internal class EntryWidgetAuthenticatedContactViewHolder(
+    private val binding: EntryWidgetAuthenticatedContactBinding,
+    itemTheme: MediaTypeItemTheme?
+) : EntryWidgetAdapter.ViewHolder(binding.root) {
+
+    init {
+        itemTheme?.let {
+            binding.root.applyLayerTheme(it.background)
+            binding.title.applyTextTheme(it.title)
+            binding.description.applyTextTheme(it.message)
+            binding.icon.applyImageColorTheme(it.iconColor)
+            it.loadingTintColor?.primaryColorStateList?.let { tintList ->
+                binding.iconLoading.backgroundTintList = tintList
+                binding.titleLoading.backgroundTintList = tintList
+                binding.descriptionLoading.backgroundTintList = tintList
+            }
+        }
+    }
+
+    override fun bind(itemType: EntryWidgetContract.ItemType, onClickListener: View.OnClickListener) {
+        binding.root.setOnClickListener(onClickListener)
+        binding.root.contentDescription = null
+        binding.icon.setImageResource(R.drawable.ic_secure_message)
+        binding.title.setLocaleText(R.string.entry_widget_secure_messaging_button_label)
+        binding.description.setLocaleText(R.string.entry_widget_secure_messaging_button_description)
+        binding.description.setLocaleHint(R.string.entry_widget_secure_messaging_button_accessibility_hint)
+        binding.unreadMessagesBadge.visibility = View.VISIBLE // TODO: implement
+        binding.unreadMessagesBadge.text = "35" // TODO: implement
+        binding.loadingGroup.isVisible = itemType == EntryWidgetContract.ItemType.LOADING_STATE
+    }
+}

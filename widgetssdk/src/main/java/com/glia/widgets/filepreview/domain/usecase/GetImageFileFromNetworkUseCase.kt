d58f68bae62cfbfb97762ddd8631fb140d203099
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-11-07 17:39:00 +0200

    Apply SC business logic to the Chat screen

--- widgetssdk/src/main/java/com/glia/widgets/filepreview/domain/usecase/GetImageFileFromNetworkUseCase.kt
+++ widgetssdk/src/main/java/com/glia/widgets/filepreview/domain/usecase/GetImageFileFromNetworkUseCase.kt
@@ -3,6 +3,7 @@ package com.glia.widgets.filepreview.domain.usecase
 import android.graphics.Bitmap
 import com.glia.androidsdk.chat.AttachmentFile
 import com.glia.widgets.chat.domain.DecodeSampledBitmapFromInputStreamUseCase
+import com.glia.widgets.core.secureconversations.domain.ManageSecureMessagingStatusUseCase
 import com.glia.widgets.filepreview.data.GliaFileRepository
 import com.glia.widgets.filepreview.domain.exception.FileNameMissingException
 import com.glia.widgets.filepreview.domain.exception.RemoteFileIsDeletedException
@@ -11,12 +12,13 @@ import io.reactivex.rxjava3.core.Maybe
 
 internal class GetImageFileFromNetworkUseCase(
     private val gliaFileRepository: GliaFileRepository,
-    private val decodeSampledBitmapFromInputStreamUseCase: DecodeSampledBitmapFromInputStreamUseCase
+    private val decodeSampledBitmapFromInputStreamUseCase: DecodeSampledBitmapFromInputStreamUseCase,
+    private val manageSecureMessagingStatusUseCase: ManageSecureMessagingStatusUseCase
 ) {
     operator fun invoke(file: AttachmentFile?): Maybe<Bitmap> = when {
         file?.name.isNullOrBlank() -> Maybe.error(FileNameMissingException())
         file!!.isDeleted -> Maybe.error(RemoteFileIsDeletedException())
-        else -> gliaFileRepository.loadImageFileFromNetwork(file)
+        else -> gliaFileRepository.loadImageFileFromNetwork(manageSecureMessagingStatusUseCase.shouldUseSecureMessagingEndpoints(), file)
             .flatMap { decodeSampledBitmapFromInputStreamUseCase(it) }
             .doOnSuccess { gliaFileRepository.putImageToCache(file.fileName, it) }
     }

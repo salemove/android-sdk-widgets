16badaea31881065512d213ccc62b63ea29bb9a9
Andriy Shevtsov <andriy.shevtsov@masterofcode.com>
2024-11-08 00:29:36 +0200

    Show unread messages counter for 'Messaging' on embedded view Implement unread messages counter logic
    MOB-3746

---
+++ widgetssdk/src/main/java/com/glia/widgets/core/secureconversations/domain/ObserveUnreadMessagesCountUseCase.kt
@@ -0,0 +1,52 @@
+package com.glia.widgets.core.secureconversations.domain
+
+import com.glia.androidsdk.GliaException
+import com.glia.androidsdk.RequestCallback
+import com.glia.widgets.chat.domain.IsAuthenticatedUseCase
+import com.glia.widgets.core.secureconversations.SecureConversationsRepository
+import com.glia.widgets.di.GliaCore
+import com.glia.widgets.helper.Logger
+import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.TimeUnit
+
+private const val TAG = "ObserveUnreadMessagesCountUseCase"
+
+internal class ObserveUnreadMessagesCountUseCase(
+    private val repository: SecureConversationsRepository,
+    private val isAuthenticatedUseCase: IsAuthenticatedUseCase,
+    private val core: GliaCore
+) {
+
+    /**
+     * This function provides a default value instead of an error.
+     * If error handling is needed, use the repository function directly.
+     *
+     * @return [NO_UNREAD_MESSAGES] if the current socket doesn't signal a success value within the specified [TIMEOUT_SEC] window.
+     *
+     * This is combined with the chat transcript result to avoid "Jumping UI" when adding new messages'
+     * divider [SecureConversationsRepository.getUnreadMessagesCount] is a socket call and
+     * there is no warranty that it will return anything so timeout is added to make sure that it will return [NO_UNREAD_MESSAGES]
+     * after the timeout if there is no answer from socket yet.
+     */
+    operator fun invoke(): Observable<Int> = when {
+        core.isInitialized.not() -> Observable.create { it.onNext(NO_UNREAD_MESSAGES) }
+        isAuthenticatedUseCase().not() -> Observable.create { it.onNext(NO_UNREAD_MESSAGES) }
+        else -> observeUnreadMessagesCount(repository)
+    }
+
+    private fun observeUnreadMessagesCount(repository: SecureConversationsRepository): Observable<Int> {
+        return Observable.create { emitter ->
+            repository.getUnreadMessagesCount(object : RequestCallback<Int> {
+                override fun onResult(count: Int?, exception: GliaException?) {
+                    Logger.d(TAG, "onResult")
+                    if (emitter.isDisposed) return
+                    if (exception != null) {
+                        emitter.tryOnError(exception)
+                    } else {
+                        emitter.onNext(count ?: NO_UNREAD_MESSAGES)
+                    }
+                }
+            })
+        }.timeout(TIMEOUT_SEC, TimeUnit.SECONDS).onErrorReturnItem(NO_UNREAD_MESSAGES)
+    }
+}

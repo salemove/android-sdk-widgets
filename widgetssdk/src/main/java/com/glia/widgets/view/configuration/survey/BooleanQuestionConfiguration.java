daf7f826c00bb1e0e7f583f80aa1e782e320df23
Aleksandr Chatsky <aleksandr.chatsky@glia.com>
2024-11-07 15:36:31 +0200

    Update glia color names in internal code to match designs
    The goal is to make it less confusing which color to apply when working with Figma design mocks

--- widgetssdk/src/main/java/com/glia/widgets/view/configuration/survey/BooleanQuestionConfiguration.java
+++ widgetssdk/src/main/java/com/glia/widgets/view/configuration/survey/BooleanQuestionConfiguration.java
@@ -78,8 +78,8 @@ public class BooleanQuestionConfiguration implements Parcelable {
 
         @SuppressLint("ResourceType")
         private OptionButtonConfiguration prepareDefaultButtonConfiguration(ResourceProvider resourceProvider) {
-            ColorStateList normalTextColor = resourceProvider.getColorStateList(R.color.glia_base_dark_color);
-            ColorStateList selectedTextColor = resourceProvider.getColorStateList(R.color.glia_base_light_color);
+            ColorStateList normalTextColor = resourceProvider.getColorStateList(R.color.glia_dark_color);
+            ColorStateList selectedTextColor = resourceProvider.getColorStateList(R.color.glia_light_color);
             TextConfiguration normalText = new TextConfiguration.Builder()
                     .textColor(normalTextColor)
                     .textSize(resourceProvider.getDimension(R.dimen.glia_survey_default_text_size))
@@ -94,18 +94,18 @@ public class BooleanQuestionConfiguration implements Parcelable {
                     .build(resourceProvider);
 
             LayerConfiguration normalLayer = new LayerConfiguration.Builder()
-                    .backgroundColor(resourceProvider.getString(R.color.glia_base_light_color))
+                    .backgroundColor(resourceProvider.getString(R.color.glia_light_color))
                     .borderColor(resourceProvider.getString(R.color.glia_stroke_gray))
                     .cornerRadius(resourceProvider.getDimension(R.dimen.glia_survey_default_corner_radius))
                     .build();
             LayerConfiguration selectedLayer = new LayerConfiguration.Builder()
-                    .backgroundColor(resourceProvider.getString(R.color.glia_brand_primary_color))
-                    .borderColor(resourceProvider.getString(R.color.glia_brand_primary_color))
+                    .backgroundColor(resourceProvider.getString(R.color.glia_primary_color))
+                    .borderColor(resourceProvider.getString(R.color.glia_primary_color))
                     .cornerRadius(resourceProvider.getDimension(R.dimen.glia_survey_default_corner_radius))
                     .build();
             LayerConfiguration highlightedLayer = new LayerConfiguration.Builder()
-                    .backgroundColor(resourceProvider.getString(R.color.glia_base_light_color))
-                    .borderColor(resourceProvider.getString(R.color.glia_system_negative_color))
+                    .backgroundColor(resourceProvider.getString(R.color.glia_light_color))
+                    .borderColor(resourceProvider.getString(R.color.glia_negative_color))
                     .cornerRadius(resourceProvider.getDimension(R.dimen.glia_survey_default_corner_radius))
                     .build();
             return new OptionButtonConfiguration.Builder()
@@ -119,7 +119,7 @@ public class BooleanQuestionConfiguration implements Parcelable {
         }
 
         private TextConfiguration prepareDefaultTitleConfiguration(ResourceProvider resourceProvider) {
-            ColorStateList normalTextColor = resourceProvider.getColorStateList(R.color.glia_base_dark_color);
+            ColorStateList normalTextColor = resourceProvider.getColorStateList(R.color.glia_dark_color);
             return new TextConfiguration.Builder()
                     .textColor(normalTextColor)
                     .bold(true)

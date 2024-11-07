7978396a58af3da310d929d61e179d5078199ecf
Andriy Shevtsov <andriy.shevtsov@masterofcode.com>
2024-11-08 01:13:58 +0200

    Show unread messages counter for 'Messaging' on embedded view Fix snapshot tests Add new snapshot tests Add Unified UI support for badge
    MOB-3746

--- widgetssdk/src/testSnapshot/java/com/glia/widgets/entrywidget/EntryWidgetBottomSheetTest.kt
+++ widgetssdk/src/testSnapshot/java/com/glia/widgets/entrywidget/EntryWidgetBottomSheetTest.kt
@@ -2,6 +2,7 @@ package com.glia.widgets.entrywidget
 
 import android.view.View
 import androidx.core.view.allViews
+import com.glia.widgets.core.secureconversations.domain.ObserveUnreadMessagesCountUseCase
 import com.glia.widgets.databinding.EntryWidgetFragmentBinding
 import com.glia.widgets.view.unifiedui.theme.UnifiedTheme
 
@@ -9,12 +10,20 @@ internal class EntryWidgetBottomSheetTest : EntryWidgetEmbeddedViewTest() {
     override fun setupView(
         items: List<EntryWidgetContract.ItemType>,
         viewType: EntryWidgetContract.ViewType,
+        observeUnreadMessagesCountUseCase: ObserveUnreadMessagesCountUseCase,
         unifiedTheme: UnifiedTheme?
     ): View {
+        localeProviderMock()
+
         val entryWidgetFragment = EntryWidgetFragment()
         val binding = EntryWidgetFragmentBinding.inflate(layoutInflater)
 
-        entryWidgetFragment.setupView(context, binding, unifiedTheme?.entryWidgetTheme)
+        entryWidgetFragment.setupView(
+            context,
+            binding,
+            unifiedTheme?.entryWidgetTheme,
+            observeUnreadMessagesCountUseCase,
+        )
 
         binding.container.allViews.forEach {
             val entryWidgetView = it as? EntryWidgetView

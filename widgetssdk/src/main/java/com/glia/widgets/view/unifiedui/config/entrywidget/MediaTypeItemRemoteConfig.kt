0f47b0a981dc5777e59267d1f755bc0d918b1f70
Andriy Shevtsov <andriy.shevtsov@masterofcode.com>
2024-11-08 01:13:58 +0200

    Show unread messages counter for 'Messaging' on embedded view Fix snapshot tests Add new snapshot tests Add Unified UI support for badge
    MOB-3746

--- widgetssdk/src/main/java/com/glia/widgets/view/unifiedui/config/entrywidget/MediaTypeItemRemoteConfig.kt
+++ widgetssdk/src/main/java/com/glia/widgets/view/unifiedui/config/entrywidget/MediaTypeItemRemoteConfig.kt
@@ -3,6 +3,7 @@ package com.glia.widgets.view.unifiedui.config.entrywidget
 import com.glia.widgets.view.unifiedui.config.base.ColorLayerRemoteConfig
 import com.glia.widgets.view.unifiedui.config.base.LayerRemoteConfig
 import com.glia.widgets.view.unifiedui.config.base.TextRemoteConfig
+import com.glia.widgets.view.unifiedui.config.chat.BadgeRemoteConfig
 import com.glia.widgets.view.unifiedui.theme.entrywidget.MediaTypeItemTheme
 import com.google.gson.annotations.SerializedName
 
@@ -16,13 +17,16 @@ internal data class MediaTypeItemRemoteConfig(
     @SerializedName("message")
     val message: TextRemoteConfig?,
     @SerializedName("loadingTintColor")
-    val loadingTintColor: ColorLayerRemoteConfig?
+    val loadingTintColor: ColorLayerRemoteConfig?,
+    @SerializedName("badge")
+    val badge: BadgeRemoteConfig?
 ) {
     fun toMediaTypeItemTheme(): MediaTypeItemTheme = MediaTypeItemTheme(
         background = background?.toLayerTheme(),
         iconColor = iconColor?.toColorTheme(),
         title = title?.toTextTheme(),
         message = message?.toTextTheme(),
-        loadingTintColor = loadingTintColor?.toColorTheme()
+        loadingTintColor = loadingTintColor?.toColorTheme(),
+        badge = badge?.toBadgeTheme()
     )
 }

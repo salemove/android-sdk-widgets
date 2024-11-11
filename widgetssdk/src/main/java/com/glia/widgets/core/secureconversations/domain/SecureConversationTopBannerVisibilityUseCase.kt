69b840df4009eb0fd66a16d3410eb24ec089d175
Aleksandr Chatsky <aleksandr.chatsky@glia.com>
2024-11-11 14:41:53 +0200

    Add use case for SC top banner visibility
    MOB-3639

---
+++ widgetssdk/src/main/java/com/glia/widgets/core/secureconversations/domain/SecureConversationTopBannerVisibilityUseCase.kt
@@ -0,0 +1,50 @@
+package com.glia.widgets.core.secureconversations.domain
+
+import com.glia.androidsdk.Engagement.MediaType
+import com.glia.androidsdk.queuing.Queue
+import com.glia.androidsdk.queuing.QueueState
+import com.glia.widgets.core.queue.QueueRepository
+import com.glia.widgets.core.queue.QueuesState
+import io.reactivex.rxjava3.core.Flowable
+
+/**
+ * Should show top banner in secure conversations chat only if both conditions are met
+ * - There is an open queue with text, audio or video media
+ * - Has pending secure conversations
+ */
+internal class SecureConversationTopBannerVisibilityUseCase(
+    private val queueRepository: QueueRepository,
+    private val hasPendingSc: HasPendingSecureConversationsWithTimeoutUseCase
+) {
+
+    operator fun invoke(): Flowable<Result<Boolean>> = getPendingSecureConversationState()
+        .flatMap { hasPendingSecureConversation ->
+            when (hasPendingSecureConversation) {
+                true -> checkAvailableQueues()
+                else -> Flowable.just(false)
+            }
+        }
+        .map { Result.success(it) }
+        .onErrorReturn { Result.failure(it) }
+        .distinctUntilChanged()
+
+    private fun getPendingSecureConversationState() = hasPendingSc.invoke().toFlowable()
+
+    private fun checkAvailableQueues() = queueRepository.queuesState
+            .filter { it !is QueuesState.Loading }
+            .map { receivedState ->
+                when(receivedState) {
+                    is QueuesState.Error -> throw receivedState.error
+                    else -> receivedState.queuesOrEmpty()
+                }
+            }
+            .map(::hasOpenQueueWithLiveMedia)
+
+    private fun hasOpenQueueWithLiveMedia(queues: List<Queue>): Boolean = queues.asSequence()
+        .filter { it.state.status == QueueState.Status.OPEN }
+        .any { it.state.medias.hasLiveMediaType() }
+
+
+    private fun Array<MediaType>.hasLiveMediaType() = asSequence()
+        .any { it == MediaType.TEXT || it == MediaType.AUDIO || it == MediaType.VIDEO }
+}

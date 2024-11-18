da8932e6b9d1468dfedc73acc88ded31679a69f6
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-11-18 23:26:40 +0200

    Resolve merge conflicts

--- widgetssdk/src/testSnapshot/java/com/glia/widgets/chat/adapter/holder/TapToRetryItemViewHolderSnapshotTest.kt
+++ widgetssdk/src/testSnapshot/java/com/glia/widgets/chat/adapter/holder/TapToRetryItemViewHolderSnapshotTest.kt
@@ -4,31 +4,15 @@ import android.view.View
 import com.glia.widgets.SnapshotTest
 import com.glia.widgets.UiTheme
 import com.glia.widgets.databinding.ChatTapToRetryItemLayoutBinding
-import com.glia.widgets.snapshotutils.SnapshotProviders
 import com.glia.widgets.view.unifiedui.theme.UnifiedTheme
-import org.junit.Before
 import org.junit.Test
 
-class TapToRetryItemViewHolderSnapshotTest : SnapshotTest(), SnapshotProviders {
-
-    @Before
-    override fun setUp() {
-        localeProviderMock()
-    }
+internal class TapToRetryItemViewHolderSnapshotTest : SnapshotTest() {
 
     @Test
     fun withDefaultTheme() {
         snapshot(
             setupView()
-        )
-    }
-
-    @Test
-    fun withUiTheme() {
-        snapshot(
-            setupView(
-                uiTheme = uiTheme()
-            )
         )
     }
 
@@ -45,18 +29,16 @@ class TapToRetryItemViewHolderSnapshotTest : SnapshotTest(), SnapshotProviders {
     fun withUnifiedTheme() {
         snapshot(
             setupView(
-                uiTheme = uiTheme(),
                 unifiedTheme = unifiedTheme()
             )
         )
     }
 
     private fun setupView(
-        uiTheme: UiTheme = UiTheme(),
         unifiedTheme: UnifiedTheme? = null
     ): View = TapToRetryItemViewHolder(
         ChatTapToRetryItemLayoutBinding.inflate(layoutInflater),
-        uiTheme,
+        UiTheme(),
         {},
         unifiedTheme
     ).itemView

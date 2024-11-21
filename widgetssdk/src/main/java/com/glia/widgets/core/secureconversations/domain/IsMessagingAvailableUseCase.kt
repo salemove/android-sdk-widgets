2f5f6f643be1871abdd7e4a998b5be80a59b5a61
Davit Dolmazyan <davit.dolmazyan@glia.com>
2024-11-21 23:16:53 +0200

    Devs want to implement GVA(Live engagement) to SC transfer

--- widgetssdk/src/main/java/com/glia/widgets/core/secureconversations/domain/IsMessagingAvailableUseCase.kt
+++ widgetssdk/src/main/java/com/glia/widgets/core/secureconversations/domain/IsMessagingAvailableUseCase.kt
@@ -4,19 +4,22 @@ import com.glia.androidsdk.queuing.Queue
 import com.glia.androidsdk.queuing.QueueState
 import com.glia.widgets.core.queue.QueueRepository
 import com.glia.widgets.core.queue.QueuesState
+import com.glia.widgets.engagement.EngagementRepository
+import com.glia.widgets.engagement.State
 import com.glia.widgets.helper.supportMessaging
 import io.reactivex.rxjava3.core.Flowable
 
-internal class IsMessagingAvailableUseCase(private val queueRepository: QueueRepository) {
+internal class IsMessagingAvailableUseCase(private val queueRepository: QueueRepository, private val engagementRepository: EngagementRepository) {
 
-    operator fun invoke(): Flowable<Result<Boolean>> = queueRepository.queuesState
-        .filter { it !is QueuesState.Loading }
-        .map { mapResult(it) }
-        .distinctUntilChanged()
+    operator fun invoke(): Flowable<Result<Boolean>> =
+        Flowable.combineLatest(queueRepository.queuesState, engagementRepository.engagementState) { queueState, engagementState ->
+            mapResult(queueState, engagementState)
+        }.distinctUntilChanged()
 
-    private fun mapResult(queueMonitorState: QueuesState): Result<Boolean> = when (queueMonitorState) {
-        is QueuesState.Error -> Result.failure(queueMonitorState.error)
-        is QueuesState.Queues -> Result.success(isMessagingAvailable(queueMonitorState.queues))
+    private fun mapResult(queuesState: QueuesState, engagementState: State): Result<Boolean> = when {
+        engagementState is State.TransferredToSecureConversation -> Result.success(true)
+        queuesState is QueuesState.Queues -> Result.success(isMessagingAvailable(queuesState.queues))
+        queuesState is QueuesState.Error -> Result.failure(queuesState.error)
         else -> Result.success(false)
     }
 

020c6424564c294abb146215e1ab8b70773c91d1
Andrii Horishnii <andrii.horishnii@masterofcode.com>
2024-12-18 00:12:16 +0200

    Merge mark messages as read logic with leave SC
    Do not start a timer for marking messages as read if the leave secure conversation dialog is shown.
    
    MOB-3833

--- widgetssdk/src/test/java/com/glia/widgets/core/secureconversations/domain/MarkMessagesReadWithDelayUseCaseTest.kt
+++ widgetssdk/src/test/java/com/glia/widgets/core/secureconversations/domain/MarkMessagesReadWithDelayUseCaseTest.kt
@@ -2,7 +2,9 @@ package com.glia.widgets.core.secureconversations.domain
 
 import com.glia.androidsdk.RequestCallback
 import com.glia.widgets.core.secureconversations.SecureConversationsRepository
+import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.plugins.RxJavaPlugins
+import io.reactivex.rxjava3.processors.BehaviorProcessor
 import io.reactivex.rxjava3.schedulers.TestScheduler
 import org.junit.After
 import org.junit.Before
@@ -10,6 +12,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.kotlin.any
 import org.mockito.kotlin.doAnswer
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
@@ -42,8 +45,32 @@ class MarkMessagesReadWithDelayUseCaseTest {
             val callback: RequestCallback<Void> = it.getArgument(0)
             callback.onResult(null, null)
         }.whenever(repository).markMessagesRead(any())
+        whenever(repository.isLeaveSecureConversationDialogVisibleObservable) doReturn Flowable.just(false)
 
         val testObservable = useCase().test()
+        verify(repository, never()).markMessagesRead(any())
+        testScheduler.advanceTimeBy(DELAY_SEC, TimeUnit.SECONDS)
+        verify(repository).markMessagesRead(any())
+
+        testObservable.assertComplete()
+    }
+
+    @Test
+    fun `invoke calls mark messages read after leave SC dialog closed with expected delay`() {
+        val testScheduler = TestScheduler()
+        RxJavaPlugins.setComputationSchedulerHandler { testScheduler }
+
+        doAnswer {
+            val callback: RequestCallback<Void> = it.getArgument(0)
+            callback.onResult(null, null)
+        }.whenever(repository).markMessagesRead(any())
+        val isLeaveSecureConversationDialogVisibleBehaviorProcessor = BehaviorProcessor.createDefault(true)
+        whenever(repository.isLeaveSecureConversationDialogVisibleObservable) doReturn isLeaveSecureConversationDialogVisibleBehaviorProcessor
+
+        val testObservable = useCase().test()
+        testScheduler.advanceTimeBy(DELAY_SEC, TimeUnit.SECONDS)
+        verify(repository, never()).markMessagesRead(any())
+        isLeaveSecureConversationDialogVisibleBehaviorProcessor.onNext(false)
         verify(repository, never()).markMessagesRead(any())
         testScheduler.advanceTimeBy(DELAY_SEC, TimeUnit.SECONDS)
         verify(repository).markMessagesRead(any())

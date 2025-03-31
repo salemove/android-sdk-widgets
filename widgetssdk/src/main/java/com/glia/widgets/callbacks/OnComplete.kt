7a956e502eec938e72718c57040ccd3ab30e3e09
Andriy Shevtsov <andriy.shevtsov@masterofcode.com>
2025-03-31 20:12:44 +0300

    Add Widgets public classes for Core SDK classes: GliaException and RequestCallback
    MOB-4213

---
+++ widgetssdk/src/main/java/com/glia/widgets/callbacks/OnComplete.kt
@@ -0,0 +1,12 @@
+package com.glia.widgets.callbacks
+
+/**
+ * Callback used in some requests to Glia Widgets SDK.
+ *
+</T> */
+fun interface OnComplete {
+    /**
+     * Function that is fired if request completes
+     */
+    fun onComplete()
+}

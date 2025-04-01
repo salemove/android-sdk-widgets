13e17ad3a25634c500a4fedae803a0b3e41ca8fa
Andriy Shevtsov <andriy.shevtsov@masterofcode.com>
2025-04-01 20:57:43 +0300

    Add getVisitorInfo() to Android Widgets SDK Add updateVisitorInfo() to Android Widgets SDK
    MOB-4203
    MOB-4204

---
+++ widgetssdk/src/main/java/com/glia/widgets/core/visitor/VisitorInfo.kt
@@ -0,0 +1,32 @@
+package com.glia.widgets.core.visitor
+
+/**
+ * Provide Visitor information
+ *
+ * @see com.glia.widgets.GliaWidgets.getVisitorInfo()
+ */
+data class VisitorInfo(
+    val name: String? = null,
+    val email: String? = null,
+    val phone: String? = null,
+    val note: String? = null,
+    val customAttributesMap: Map<String, String>,
+    val generatedName: String? = null,
+    val banned: Boolean? = null,
+    val href: String? = null,
+    val id: String? = null,
+    val externalId: String? = null,
+) {
+    constructor(visitorInfo: com.glia.androidsdk.visitor.VisitorInfo) : this(
+        visitorInfo.name,
+        visitorInfo.email,
+        visitorInfo.phone,
+        visitorInfo.note,
+        visitorInfo.customAttributesMap,
+        visitorInfo.generatedName,
+        visitorInfo.banned,
+        visitorInfo.href,
+        visitorInfo.id,
+        visitorInfo.externalId
+    )
+}

320dff4d1ae9377a7e3e47961c9446614a27a9be
Andriy Shevtsov <andriy.shevtsov@masterofcode.com>
2025-04-17 19:43:22 +0300

    Add CustomCardMessage to Android Widgets SDK CustomCardMessage is based on Core SDK's ChatMessage and iOS MessageRenderer
    MOB-4209

--- widgetssdk/src/test/java/com/glia/widgets/chat/domain/AppendHistoryCustomCardItemUseCaseTest.kt
+++ widgetssdk/src/test/java/com/glia/widgets/chat/domain/AppendHistoryCustomCardItemUseCaseTest.kt
@@ -1,7 +1,9 @@
 package com.glia.widgets.chat.domain
 
+import com.glia.androidsdk.chat.Chat
 import com.glia.androidsdk.chat.OperatorMessage
 import com.glia.androidsdk.chat.SingleChoiceAttachment
+import com.glia.widgets.chat.adapter.CustomCardMessage
 import com.glia.widgets.chat.model.ChatItem
 import com.glia.widgets.chat.model.CustomCardChatItem
 import com.glia.widgets.chat.model.VisitorMessageItem
@@ -9,6 +11,7 @@ import org.junit.After
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito.mockStatic
 import org.mockito.kotlin.any
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
@@ -21,6 +24,7 @@ class AppendHistoryCustomCardItemUseCaseTest {
     private lateinit var customCardShouldShowUseCase: CustomCardShouldShowUseCase
 
     private lateinit var useCase: AppendHistoryCustomCardItemUseCase
+    private lateinit var staticMock: AutoCloseable
 
     @Before
     fun setUp() {
@@ -30,18 +34,25 @@ class AppendHistoryCustomCardItemUseCaseTest {
         whenever(customCardTypeUseCase(any())) doReturn 100
 
         useCase = AppendHistoryCustomCardItemUseCase(customCardTypeUseCase, customCardShouldShowUseCase)
+        staticMock = mockStatic(CustomCardMessage::class.java)
     }
 
     @After
     fun tearDown() {
         items.clear()
+        staticMock.close()
     }
 
     @Test
     fun `invoke adds CustomCardChatItem when customCardShouldShowUseCase returns true`() {
         whenever(customCardShouldShowUseCase.execute(any(), any(), any())) doReturn true
+        val chatMessage: OperatorMessage = mock()
+        whenever(chatMessage.id) doReturn "id"
+        whenever(chatMessage.content) doReturn ""
+        whenever(chatMessage.senderType) doReturn Chat.Participant.OPERATOR
+        whenever(chatMessage.timestamp) doReturn -1
 
-        useCase(items, mock(), 120)
+        useCase(items, chatMessage, 120)
 
         assertTrue(items.isNotEmpty())
         assertTrue(items.first() is CustomCardChatItem)
@@ -55,6 +66,8 @@ class AppendHistoryCustomCardItemUseCaseTest {
 
         whenever(chatMessage.attachment) doReturn singleChoiceAttachment
         whenever(chatMessage.id) doReturn "id"
+        whenever(chatMessage.content) doReturn ""
+        whenever(chatMessage.senderType) doReturn Chat.Participant.OPERATOR
         whenever(chatMessage.timestamp) doReturn -1
 
         whenever(customCardShouldShowUseCase.execute(any(), any(), any())) doReturn true

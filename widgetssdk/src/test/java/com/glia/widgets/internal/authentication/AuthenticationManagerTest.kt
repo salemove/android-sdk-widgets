4a44bca25077609cc476346da751663f654276b1
Davit Dolmazyan <davit.dolmazyan@glia.com>
2025-06-09 23:25:11 +0300

    Add tests for AuthenticationManager.kt and move PushClickHandler call out of the success block

--- widgetssdk/src/test/java/com/glia/widgets/internal/authentication/AuthenticationManagerTest.kt
+++ widgetssdk/src/test/java/com/glia/widgets/internal/authentication/AuthenticationManagerTest.kt
@@ -1,140 +1,261 @@
 package com.glia.widgets.internal.authentication
 
+import android.mock
+import android.unMock
+import com.glia.androidsdk.GliaException
 import com.glia.androidsdk.RequestCallback
 import com.glia.widgets.authentication.Authentication
-import junit.framework.TestCase.assertEquals
-import org.junit.Assert.assertNotNull
-import org.junit.Assert.assertTrue
+import com.glia.widgets.callbacks.OnComplete
+import com.glia.widgets.callbacks.OnError
+import com.glia.widgets.di.Dependencies
+import com.glia.widgets.helper.Logger
+import com.glia.widgets.internal.secureconversations.SecureConversationsRepository
+import com.glia.widgets.push.notifications.PushClickHandlerController
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
+import junit.framework.TestCase.assertTrue
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
-import org.mockito.kotlin.any
-import org.mockito.kotlin.eq
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.verify
-import org.mockito.kotlin.whenever
+import com.glia.androidsdk.visitor.Authentication as CoreAuthentication
 
-class AuthenticationManagerTest {
-    @Test
-    fun toCoreType_setBehavior_returnsCoreAuthenticationWithCorrectBehavior() {
-        val widgetAuthentication = mock<AuthenticationManager>()
-        val coreAuthentication = widgetAuthentication.toCoreType()
+internal class AuthenticationManagerTest {
 
-        val behavior = com.glia.androidsdk.visitor.Authentication.Behavior.FORBIDDEN_DURING_ENGAGEMENT
-        coreAuthentication.setBehavior(behavior)
+    private lateinit var coreAuthentication: CoreAuthentication
+    private lateinit var onAuthenticationRequestCallback: () -> Unit
 
-        verify(widgetAuthentication).setBehavior(behavior.toWidgetsType())
+    private lateinit var secureConversationsRepository: SecureConversationsRepository
+
+    private lateinit var authenticationManager: AuthenticationManager
+    private lateinit var pushClickHandlerController: PushClickHandlerController
+
+
+    @Before
+    fun setUp() {
+        coreAuthentication = mockk(relaxUnitFun = true)
+        onAuthenticationRequestCallback = mockk<() -> Unit>(relaxed = true)
+        secureConversationsRepository = mockk(relaxUnitFun = true)
+        pushClickHandlerController = mockk(relaxUnitFun = true)
+        Logger.mock()
+        Dependencies.mock()
+
+        every { Dependencies.repositoryFactory.secureConversationsRepository } returns secureConversationsRepository
+        every { Dependencies.controllerFactory.pushClickHandlerController } returns pushClickHandlerController
+
+        authenticationManager = AuthenticationManager(coreAuthentication, onAuthenticationRequestCallback)
+    }
+
+    @After
+    fun tearDown() {
+        Logger.unMock()
+        Dependencies.unMock()
     }
 
     @Test
-    fun toCoreType_authenticateWithValidJwtToken_callsAuthenticateOnWidgetAuthentication() {
-        val widgetAuthentication = mock<AuthenticationManager>()
-        val coreAuthentication = widgetAuthentication.toCoreType()
-
-        val jwtToken = "validToken"
-        val externalAccessToken = "externalToken"
-        val callback = mock<RequestCallback<Void>>()
-
-        coreAuthentication.authenticate(jwtToken, externalAccessToken, callback)
-
-        verify(widgetAuthentication).authenticate(eq(jwtToken), eq(externalAccessToken), any(), any())
+    fun `isAuthenticated calls corresponding function from core`() {
+        every { coreAuthentication.isAuthenticated } returns true
+        assertTrue(authenticationManager.isAuthenticated)
+        verify { coreAuthentication.isAuthenticated }
     }
 
     @Test
-    fun toCoreType_authenticateWithoutCallback_callsAuthenticateOnWidgetAuthentication() {
-        val widgetAuthentication = mock<AuthenticationManager>()
-        val coreAuthentication = widgetAuthentication.toCoreType()
-
-        val jwtToken = "validToken"
-        val externalAccessToken = "externalToken"
-
-        coreAuthentication.authenticate(jwtToken, externalAccessToken, null)
-
-        verify(widgetAuthentication).authenticate(eq(jwtToken), eq(externalAccessToken), any(), any())
+    fun `setBehavior calls corresponding core function`() {
+        authenticationManager.setBehavior(Authentication.Behavior.ALLOWED_DURING_ENGAGEMENT)
+        verify { coreAuthentication.setBehavior(eq(CoreAuthentication.Behavior.ALLOWED_DURING_ENGAGEMENT)) }
     }
 
     @Test
-    fun toCoreType_deauthenticate_callsDeauthenticateOnWidgetAuthentication() {
-        val widgetAuthentication = mock<AuthenticationManager>()
-        val coreAuthentication = widgetAuthentication.toCoreType()
+    fun `authenticate triggers only the success callback when core authentication succeeds`() {
+        every { Dependencies.destroyControllersAndResetQueueing() } just Runs
+        val token = "just token"
+        val externalAccessToken = "external"
+        val onComplete = mockk<OnComplete>(relaxUnitFun = true)
+        val onError = mockk<OnError>(relaxUnitFun = true)
+        val authCallbackSlot = slot<RequestCallback<Void>>()
 
-        val callback = mock<RequestCallback<Void>>()
+        authenticationManager.authenticate(token, externalAccessToken, onComplete, onError)
 
-        coreAuthentication.deauthenticate(callback)
+        verify { onAuthenticationRequestCallback() }
+        verify { Dependencies.destroyControllersAndResetQueueing() }
+        verify { Logger.i(any(), any()) }
 
-        verify(widgetAuthentication).deauthenticate(any(), any())
-    }
-
-    @Test
-    fun toCoreType_deauthenticateWithoutCallback_callsDeauthenticateOnWidgetAuthentication() {
-        val widgetAuthentication = mock<AuthenticationManager>()
-        val coreAuthentication = widgetAuthentication.toCoreType()
-
-        coreAuthentication.deauthenticate(null)
-
-        verify(widgetAuthentication).deauthenticate(any(), any())
-    }
-
-    @Test
-    fun toCoreType_isAuthenticated_returnsCorrectValue() {
-        val widgetAuthentication = mock<AuthenticationManager>()
-        whenever(widgetAuthentication.isAuthenticated).thenReturn(true)
-
-        val coreAuthentication = widgetAuthentication.toCoreType()
-
-        assertTrue(coreAuthentication.isAuthenticated)
-    }
-
-    @Test
-    fun toCoreType_refreshWithValidJwtToken_callsRefreshOnWidgetAuthentication() {
-        val widgetAuthentication = mock<AuthenticationManager>()
-        val coreAuthentication = widgetAuthentication.toCoreType()
-
-        val jwtToken = "validToken"
-        val externalAccessToken = "externalToken"
-        val callback = mock<RequestCallback<Void>>()
-
-        coreAuthentication.refresh(jwtToken, externalAccessToken, callback)
-
-        verify(widgetAuthentication).refresh(eq(jwtToken), eq(externalAccessToken), any(), any())
-    }
-
-    @Test
-    fun toCoreType_refreshWithoutCallback_callsRefreshOnWidgetAuthentication() {
-        val widgetAuthentication = mock<AuthenticationManager>()
-        val coreAuthentication = widgetAuthentication.toCoreType()
-
-        val jwtToken = "validToken"
-        val externalAccessToken = "externalToken"
-
-        coreAuthentication.refresh(jwtToken, externalAccessToken, null)
-
-        verify(widgetAuthentication).refresh(eq(jwtToken), eq(externalAccessToken), any(), any())
-    }
-
-    @Test
-    fun testWidgetsAuthenticationBehaviorsCorrespondToCoreAuthenticationBehaviors() {
-        val allCoreAuthBehaviors = com.glia.androidsdk.visitor.Authentication.Behavior.entries
-        val allWidgetsAuthBehaviors = Authentication.Behavior.entries
-
-        assertEquals(allCoreAuthBehaviors.size, allWidgetsAuthBehaviors.size)
-        allWidgetsAuthBehaviors.forEachIndexed { index, item ->
-            val coreBehavior = item.toCoreType()
-
-            assertNotNull(coreBehavior)
-            assertEquals(coreBehavior.name, allWidgetsAuthBehaviors[index].name)
+        verify {
+            coreAuthentication.authenticate(eq(token), eq(externalAccessToken), capture(authCallbackSlot))
         }
+
+        //verify that all the business logic is inside callback
+
+        //callback
+        verify(exactly = 0) { pushClickHandlerController.onAuthenticationAttempt() }
+
+        //success
+        verify(exactly = 0) { secureConversationsRepository.subscribe() }
+        verify(exactly = 0) { onComplete.onComplete() }
+
+        //error
+        verify(exactly = 0) { onError.onError(any()) }
+
+        authCallbackSlot.captured.onResult(null, null)
+
+        //callback
+        verify { pushClickHandlerController.onAuthenticationAttempt() }
+
+        //success
+        verify { secureConversationsRepository.subscribe() }
+        verify { onComplete.onComplete() }
+
+        //error
+        verify(exactly = 0) { onError.onError(any()) }
     }
 
     @Test
-    fun testCoreAuthenticationBehaviorsCorrespondToWidgetsAuthenticationBehaviors() {
-        val allCoreAuthBehaviors = com.glia.androidsdk.visitor.Authentication.Behavior.entries
-        val allWidgetsAuthBehaviors = Authentication.Behavior.entries
+    fun `authenticate triggers only the error callback when core authentication fails`() {
+        every { Dependencies.destroyControllersAndResetQueueing() } just Runs
+        val token = "just token"
+        val externalAccessToken = "external"
+        val onComplete = mockk<OnComplete>(relaxUnitFun = true)
+        val onError = mockk<OnError>(relaxUnitFun = true)
+        val authCallbackSlot = slot<RequestCallback<Void>>()
 
-        assertEquals(allCoreAuthBehaviors.size, allWidgetsAuthBehaviors.size)
-        allCoreAuthBehaviors.forEachIndexed { index, item ->
-            val widgetsBehavior = item.toWidgetsType()
+        authenticationManager.authenticate(token, externalAccessToken, onComplete, onError)
 
-            assertNotNull(widgetsBehavior)
-            assertEquals(widgetsBehavior.name, allCoreAuthBehaviors[index].name)
+        verify { onAuthenticationRequestCallback() }
+        verify { Dependencies.destroyControllersAndResetQueueing() }
+        verify { Logger.i(any(), any()) }
+
+        verify {
+            coreAuthentication.authenticate(eq(token), eq(externalAccessToken), capture(authCallbackSlot))
         }
+
+        //verify that all the business logic is inside callback
+
+        //callback
+        verify(exactly = 0) { pushClickHandlerController.onAuthenticationAttempt() }
+
+        //success
+        verify(exactly = 0) { secureConversationsRepository.subscribe() }
+        verify(exactly = 0) { onComplete.onComplete() }
+
+        //error
+        verify(exactly = 0) { onError.onError(any()) }
+
+        authCallbackSlot.captured.onResult(null, GliaException("error", GliaException.Cause.INVALID_INPUT))
+
+        //callback
+        verify { pushClickHandlerController.onAuthenticationAttempt() }
+
+        verify(exactly = 0) { secureConversationsRepository.subscribe() }
+        verify(exactly = 0) { onComplete.onComplete() }
+
+        verify { onError.onError(any()) }
     }
+
+    @Test
+    fun `deauthenticate triggers success callback only when the core deauthentication succeeds`() {
+        val stopPushNotifications = true
+        every { Dependencies.destroyControllersAndResetEngagementData() } just Runs
+
+        val onComplete = mockk<OnComplete>(relaxUnitFun = true)
+        val onError = mockk<OnError>(relaxUnitFun = true)
+        val authCallbackSlot = slot<RequestCallback<Void>>()
+
+        authenticationManager.deauthenticate(stopPushNotifications, onComplete, onError)
+
+        verify { Logger.i(any(), any()) }
+        verify { Dependencies.destroyControllersAndResetEngagementData() }
+        verify { secureConversationsRepository.unsubscribeAndResetData() }
+        verify {
+            coreAuthentication.deauthenticate(eq(stopPushNotifications), capture(authCallbackSlot))
+        }
+
+        authCallbackSlot.captured.onResult(null, null)
+
+        verify { onComplete.onComplete() }
+
+        verify(exactly = 0) { onError.onError(any()) }
+    }
+
+    @Test
+    fun `deauthenticate triggers error callback only when the core deauthentication fails`() {
+        val stopPushNotifications = true
+        every { Dependencies.destroyControllersAndResetEngagementData() } just Runs
+
+        val onComplete = mockk<OnComplete>(relaxUnitFun = true)
+        val onError = mockk<OnError>(relaxUnitFun = true)
+        val authCallbackSlot = slot<RequestCallback<Void>>()
+
+        authenticationManager.deauthenticate(stopPushNotifications, onComplete, onError)
+
+        verify { Logger.i(any(), any()) }
+        verify { Dependencies.destroyControllersAndResetEngagementData() }
+        verify { secureConversationsRepository.unsubscribeAndResetData() }
+        verify {
+            coreAuthentication.deauthenticate(eq(stopPushNotifications), capture(authCallbackSlot))
+        }
+
+        authCallbackSlot.captured.onResult(null, GliaException("error", GliaException.Cause.INVALID_INPUT))
+
+        verify(exactly = 0) { onComplete.onComplete() }
+
+        verify { onError.onError(any()) }
+    }
+
+    @Test
+    fun `refresh triggers only the success callback when core refresh succeeds`() {
+        every { Dependencies.destroyControllersAndResetQueueing() } just Runs
+        val jwtToken = "just token"
+        val externalAccessToken = "external"
+
+        val onComplete = mockk<OnComplete>(relaxUnitFun = true)
+        val onError = mockk<OnError>(relaxUnitFun = true)
+        val authCallbackSlot = slot<RequestCallback<Void>>()
+
+        authenticationManager.refresh(jwtToken, externalAccessToken, onComplete, onError)
+
+        verify { Logger.i(any(), any()) }
+        verify {
+            coreAuthentication.refresh(eq(jwtToken), eq(externalAccessToken), capture(authCallbackSlot))
+        }
+
+        //verify that all the business logic is inside callback
+        verify(exactly = 0) { onComplete.onComplete() }
+        verify(exactly = 0) { onError.onError(any()) }
+
+        authCallbackSlot.captured.onResult(null, null)
+
+        verify { onComplete.onComplete() }
+        verify(exactly = 0) { onError.onError(any()) }
+    }
+
+    @Test
+    fun `refresh triggers only the error callback when core refresh fails`() {
+        every { Dependencies.destroyControllersAndResetQueueing() } just Runs
+        val jwtToken = "just token"
+        val externalAccessToken = "external"
+
+        val onComplete = mockk<OnComplete>(relaxUnitFun = true)
+        val onError = mockk<OnError>(relaxUnitFun = true)
+        val authCallbackSlot = slot<RequestCallback<Void>>()
+
+        authenticationManager.refresh(jwtToken, externalAccessToken, onComplete, onError)
+
+        verify { Logger.i(any(), any()) }
+        verify {
+            coreAuthentication.refresh(eq(jwtToken), eq(externalAccessToken), capture(authCallbackSlot))
+        }
+
+        //verify that all the business logic is inside callback
+        verify(exactly = 0) { onComplete.onComplete() }
+        verify(exactly = 0) { onError.onError(any()) }
+
+        authCallbackSlot.captured.onResult(null, GliaException("error", GliaException.Cause.INVALID_INPUT))
+
+        verify(exactly = 0) { onComplete.onComplete() }
+        verify(exactly = 1) { onError.onError(any()) }
+    }
+
 }

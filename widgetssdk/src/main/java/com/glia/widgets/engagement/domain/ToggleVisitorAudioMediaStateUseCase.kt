707d3a35cfd9146a5de717c4ea7e49e42d05a363
Davit Dolmazyan <davit.dolmazyan@glia.com>
2025-09-23 15:55:14 +0300

    Add Call Screen telemetry

--- widgetssdk/src/main/java/com/glia/widgets/engagement/domain/ToggleVisitorAudioMediaStateUseCase.kt
+++ widgetssdk/src/main/java/com/glia/widgets/engagement/domain/ToggleVisitorAudioMediaStateUseCase.kt
@@ -1,9 +1,12 @@
 package com.glia.widgets.engagement.domain
 
 import com.glia.androidsdk.comms.Media
+import com.glia.telemetry_lib.ButtonNames
+import com.glia.telemetry_lib.GliaLogger
 import com.glia.widgets.engagement.EngagementRepository
 import com.glia.widgets.helper.Logger
 import com.glia.widgets.helper.TAG
+import com.glia.widgets.helper.logCallScreenButtonClicked
 
 internal interface ToggleVisitorAudioMediaStateUseCase {
     operator fun invoke()
@@ -12,10 +15,18 @@ internal interface ToggleVisitorAudioMediaStateUseCase {
 internal class ToggleVisitorAudioMediaStateUseCaseImpl(private val repository: EngagementRepository) : ToggleVisitorAudioMediaStateUseCase {
     override fun invoke() {
         val status: Media.Status = repository.visitorCurrentMediaState?.audio?.status ?: return
+
         when (status) {
-            Media.Status.PLAYING -> repository.muteVisitorAudio()
-            Media.Status.PAUSED -> repository.unMuteVisitorAudio()
+            Media.Status.PLAYING -> {
+                repository.muteVisitorAudio()
+                GliaLogger.logCallScreenButtonClicked(ButtonNames.MUTE)
+            }
+            Media.Status.PAUSED -> {
+                repository.unMuteVisitorAudio()
+                GliaLogger.logCallScreenButtonClicked(ButtonNames.UNMUTE)
+            }
             Media.Status.DISCONNECTED -> Logger.d(TAG, "Visitor Audio is disconnected")
         }
+
     }
 }
--- widgetssdk/src/main/java/com/glia/widgets/engagement/domain/ToggleVisitorVideoMediaStateUseCase.kt
+++ widgetssdk/src/main/java/com/glia/widgets/engagement/domain/ToggleVisitorVideoMediaStateUseCase.kt
@@ -1,9 +1,12 @@
 package com.glia.widgets.engagement.domain
 
 import com.glia.androidsdk.comms.Media
+import com.glia.telemetry_lib.ButtonNames
+import com.glia.telemetry_lib.GliaLogger
 import com.glia.widgets.engagement.EngagementRepository
 import com.glia.widgets.helper.Logger
 import com.glia.widgets.helper.TAG
+import com.glia.widgets.helper.logCallScreenButtonClicked
 
 internal interface ToggleVisitorVideoMediaStateUseCase {
     operator fun invoke()
@@ -13,8 +16,14 @@ internal class ToggleVisitorVideoMediaStateUseCaseImpl(private val repository: E
     override fun invoke() {
         val status: Media.Status = repository.visitorCurrentMediaState?.video?.status ?: return
         when (status) {
-            Media.Status.PLAYING -> repository.pauseVisitorVideo()
-            Media.Status.PAUSED -> repository.resumeVisitorVideo()
+            Media.Status.PLAYING -> {
+                repository.pauseVisitorVideo()
+                GliaLogger.logCallScreenButtonClicked(ButtonNames.VIDEO_OFF)
+            }
+            Media.Status.PAUSED -> {
+                repository.resumeVisitorVideo()
+                GliaLogger.logCallScreenButtonClicked(ButtonNames.VIDEO_ON)
+            }
             Media.Status.DISCONNECTED -> Logger.d(TAG, "Visitor Video is disconnected")
         }
     }
